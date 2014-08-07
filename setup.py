9864560f9af7da380e58204a5f1eca9537f14d84
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2014-08-07 21:30:08 +0200

    increase version, PEP8 beauty and add recommends

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def get_package():
 
 setup(
     name='privacyIDEA',
-    version='1.3dev0',
+    version='1.3dev1',
     description='privacyIDEA: identity, multifactor authentication, '
                 'authorization, audit',
     author='privacyidea.org',
