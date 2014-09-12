d3e699329a45e55076306b1378a1195a6a1cad02
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2014-09-12 10:55:26 +0200

    added parser for freeradius users file

--- privacyidea/lib/freeradiusparser.py
+++ privacyidea/lib/freeradiusparser.py
@@ -3,10 +3,53 @@ import codecs
 
 from pyparsing import Literal, White, Word, alphanums, CharsNotIn
 from pyparsing import Forward, Group, Optional, OneOrMore, ZeroOrMore
-from pyparsing import pythonStyleComment
+from pyparsing import pythonStyleComment, Regex
 
 
-class ClientConfParser(object):
+class BaseParser(object):
+    
+    def get(self):
+        """
+        return the grouped config
+        """
+        return
+    
+    def get_dict(self):
+        '''
+        return the client config as a dictionary.
+        '''
+        ret = {}
+        config = self.get()
+        for client in config:
+            client_config = {}
+            for attribute in client[1]:
+                client_config[attribute[0]] = attribute[1]
+            ret[client[0]] = client_config
+        return ret
+    
+    def dump(self):
+        conf = self.get()
+        for client in conf:
+            print "%s: %s" % (client[0], client[1])
+
+    def format(self, dict_config):
+        '''
+        :return: The formatted data as it would be written to a file
+        '''
+        return
+    
+    def save(self, dict_config=None, outfile=None):
+        if not outfile:
+            outfile = self.file
+        if dict_config:
+            output = self.format(dict_config)
+            f = codecs.open(outfile, 'w', 'utf-8')
+            for line in output.splitlines():
+                f.write(line + "\n")
+            f.close()
+
+
+class ClientConfParser(BaseParser):
 
     key = Word(alphanums + "_")
     client_key = Word(alphanums + "-_/.:")
@@ -52,24 +95,6 @@ class ClientConfParser(object):
         config = self.client_file.parseString(self.content)
         return config
     
-    def get_dict(self):
-        '''
-        return the client config as a dictionary.
-        '''
-        ret = {}
-        config = self.get()
-        for client in config:
-            client_config = {}
-            for attribute in client[1]:
-                client_config[attribute[0]] = attribute[1]
-            ret[client[0]] = client_config
-        return ret
-    
-    def dump(self):
-        conf = self.get()
-        for client in conf:
-            print "%s: %s" % (client[0], client[1])
-
     def format(self, dict_config):
         '''
         :return: The formatted data as it would be written to a file
@@ -83,25 +108,87 @@ class ClientConfParser(object):
             output += "}\n\n"
         return output
 
-    def save(self, dict_config=None, outfile=None):
-        if not outfile:
-            outfile = self.file
-        if dict_config:
-            output = self.format(dict_config)
-            f = codecs.open(outfile, 'w', 'utf-8')
-            for line in output.splitlines():
-                f.write(line + "\n")
-            f.close()
 
-
-class UserConfParser(object):
+class UserConfParser(BaseParser):
     
-    def __init__(self, infile="/etc/freeradius/users"):
-        self.file = infile
-        f = codecs.open(self.file, "r", "utf-8")
-        self.content = f.read()
-        f.close()
+    key = Word(alphanums + "-")
+    username = Word(alphanums + "@_.-/")
+    client_key = Word(alphanums + "-_/.:")
+    space = White().suppress()
+    comma = (",")
+    value = CharsNotIn("{}\n#, ")
+    comment = ("#")
+    # operator = ":="
+    operator = Regex(":=|==|=|\+=|!=|>|>=|<|<=|=~|!~|=\*|!\*")
+    assignment = Group(space
+                       + key
+                       + space.suppress()
+                       + operator
+                       + space.suppress()
+                       + value
+                       + ZeroOrMore(space).suppress()
+                       + ZeroOrMore(comma).suppress())
+    user_block = Forward()
+    # USERNAME key operator value
+    # <tab> key operator value
+    user_block << Group(username
+                        + space
+                        + key
+                        + operator
+                        + space
+                        + value
+                        + Group(ZeroOrMore(assignment)))
+    
+    user_file = OneOrMore(user_block).ignore(pythonStyleComment)
+    
+    file_header = """# File parsed and saved by privacyidea.\n\n"""
+    
+    def __init__(self,
+                 infile="/etc/freeradius/users",
+                 content=None):
+        self.file = None
+        if content:
+            self.content = content
+        else:
+            self.file = infile
+            f = codecs.open(self.file, "r", "utf-8")
+            self.content = f.read()
+            f.close()
+            
+    def get(self):
+        """
+        return the grouped config
         
+        something like this:
+        [
+        ['DEFAULT', 'Framed-Protocol', '==', 'PPP', [['Framed-Protocol', '=', 'PPP'], ['Framed-Compression', '=', 'Van-Jacobson-TCP-IP']]],
+        ['DEFAULT', 'Hint', '==', '"CSLIP"', [['Framed-Protocol', '=', 'SLIP'], ['Framed-Compression', '=', 'Van-Jacobson-TCP-IP']]],
+        ['DEFAULT', 'Hint', '==', '"SLIP"', [['Framed-Protocol', '=', 'SLIP']]]
+        ]
+        """
+        config = self.user_file.parseString(self.content)
+        return config
+    
+    def format(self, config):
+        '''
+        :return: The formatted data as it would be written to a file
+        '''
+        output = ""
+        output += self.file_header
+        for user in config:
+            output += "%s %s %s %s\n" % (user[0], user[1], user[2], user[3])
+            if len(user[4]):
+                i = 0
+                for reply_item in user[4]:
+                    i += 1
+                    output += "\t%s %s %s" % (reply_item[0],
+                                              reply_item[1],
+                                              reply_item[2])
+                    if i < len(user[4]):
+                        output += ","
+                    output += "\n"
+            output += "\n"
+        return output
         
 
 def main():
--- privacyidea/tests/functional/test_appliance_parser.py
+++ privacyidea/tests/functional/test_appliance_parser.py
@@ -22,6 +22,7 @@ log = logging.getLogger(__name__)
 import unittest
 import os
 from privacyidea.lib.freeradiusparser import ClientConfParser
+from privacyidea.lib.freeradiusparser import UserConfParser
 
 CLIENT_CONF = """# This is a client.conf for freeradius
 client localhost {
@@ -163,6 +164,249 @@ client 127.0.0.1 {
 
 """
 
+USER_CONF_A = """
+DEFAULT Auth-Type := perl
+"""
+
+USER_CONF_B = """
+administrator Cleartext-Password := "secret"
+"""
+
+USER_CONF_C = """
+user somekey == value
+"""
+USER_CONF_D = """
+DEFAULT    Hint == "SLIP"
+    Framed-Protocol = SLIP
+"""
+
+USER_CONF_E = """
+DEFAULT    Hint == "SLIP"
+    Framed-Protocol = SLIP,
+    Something = Else
+"""
+
+USER_CONF_1 = """
+#
+#    Please read the documentation file ../doc/processing_users_file,
+#    or 'man 5 users' (after installing the server) for more information.
+#
+#    This file contains authentication security and configuration
+#    information for each user.  Accounting requests are NOT processed
+#    through this file.  Instead, see 'acct_users', in this directory.
+#
+#    The first field is the user's name and can be up to
+#    253 characters in length.  This is followed (on the same line) with
+#    the list of authentication requirements for that user.  This can
+#    include password, comm server name, comm server port number, protocol
+#    type (perhaps set by the "hints" file), and huntgroup name (set by
+#    the "huntgroups" file).
+#
+#    If you are not sure why a particular reply is being sent by the
+#    server, then run the server in debugging mode (radiusd -X), and
+#    you will see which entries in this file are matched.
+#
+#    When an authentication request is received from the comm server,
+#    these values are tested. Only the first match is used unless the
+#    "Fall-Through" variable is set to "Yes".
+#
+#    A special user named "DEFAULT" matches on all usernames.
+#    You can have several DEFAULT entries. All entries are processed
+#    in the order they appear in this file. The first entry that
+#    matches the login-request will stop processing unless you use
+#    the Fall-Through variable.
+#
+#    If you use the database support to turn this file into a .db or .dbm
+#    file, the DEFAULT entries _have_ to be at the end of this file and
+#    you can't have multiple entries for one username.
+#
+#    Indented (with the tab character) lines following the first
+#    line indicate the configuration values to be passed back to
+#    the comm server to allow the initiation of a user session.
+#    This can include things like the PPP configuration values
+#    or the host to log the user onto.
+#
+#    You can include another `users' file with `$INCLUDE users.other'
+#
+
+#
+#    For a list of RADIUS attributes, and links to their definitions,
+#    see:
+#
+#    http://www.freeradius.org/rfc/attributes.html
+#
+
+#
+# Deny access for a specific user.  Note that this entry MUST
+# be before any other 'Auth-Type' attribute which results in the user
+# being authenticated.
+#
+# Note that there is NO 'Fall-Through' attribute, so the user will not
+# be given any additional resources.
+#
+#lameuser    Auth-Type := Reject
+#        Reply-Message = "Your account has been disabled."
+
+#
+# Deny access for a group of users.
+#
+# Note that there is NO 'Fall-Through' attribute, so the user will not
+# be given any additional resources.
+#
+#DEFAULT    Group == "disabled", Auth-Type := Reject
+#        Reply-Message = "Your account has been disabled."
+#
+
+#
+# This is a complete entry for "steve". Note that there is no Fall-Through
+# entry so that no DEFAULT entry will be used, and the user will NOT
+# get any attributes in addition to the ones listed here.
+#
+#steve    Cleartext-Password := "testing"
+#    Service-Type = Framed-User,
+#    Framed-Protocol = PPP,
+#    Framed-IP-Address = 172.16.3.33,
+#    Framed-IP-Netmask = 255.255.255.0,
+#    Framed-Routing = Broadcast-Listen,
+#    Framed-Filter-Id = "std.ppp",
+#    Framed-MTU = 1500,
+#    Framed-Compression = Van-Jacobsen-TCP-IP
+
+#
+# This is an entry for a user with a space in their name.
+# Note the double quotes surrounding the name.
+#
+#"John Doe"    Cleartext-Password := "hello"
+#        Reply-Message = "Hello, %{User-Name}"
+
+#
+# Dial user back and telnet to the default host for that port
+#
+#Deg    Cleartext-Password := "ge55ged"
+#    Service-Type = Callback-Login-User,
+#    Login-IP-Host = 0.0.0.0,
+#    Callback-Number = "9,5551212",
+#    Login-Service = Telnet,
+#    Login-TCP-Port = Telnet
+
+#
+# Another complete entry. After the user "dialbk" has logged in, the
+# connection will be broken and the user will be dialed back after which
+# he will get a connection to the host "timeshare1".
+#
+#dialbk    Cleartext-Password := "callme"
+#    Service-Type = Callback-Login-User,
+#    Login-IP-Host = timeshare1,
+#    Login-Service = PortMaster,
+#    Callback-Number = "9,1-800-555-1212"
+
+#
+# user "swilson" will only get a static IP number if he logs in with
+# a framed protocol on a terminal server in Alphen (see the huntgroups file).
+#
+# Note that by setting "Fall-Through", other attributes will be added from
+# the following DEFAULT entries
+#
+#swilson    Service-Type == Framed-User, Huntgroup-Name == "alphen"
+#        Framed-IP-Address = 192.168.1.65,
+#        Fall-Through = Yes
+
+#
+# If the user logs in as 'username.shell', then authenticate them
+# using the default method, give them shell access, and stop processing
+# the rest of the file.
+#
+#DEFAULT    Suffix == ".shell"
+#        Service-Type = Login-User,
+#        Login-Service = Telnet,
+#        Login-IP-Host = your.shell.machine
+
+
+#
+# The rest of this file contains the several DEFAULT entries.
+# DEFAULT entries match with all login names.
+# Note that DEFAULT entries can also Fall-Through (see first entry).
+# A name-value pair from a DEFAULT entry will _NEVER_ override
+# an already existing name-value pair.
+#
+
+#
+# Set up different IP address pools for the terminal servers.
+# Note that the "+" behind the IP address means that this is the "base"
+# IP address. The Port-Id (S0, S1 etc) will be added to it.
+#
+#DEFAULT    Service-Type == Framed-User, Huntgroup-Name == "alphen"
+#        Framed-IP-Address = 192.168.1.32+,
+#        Fall-Through = Yes
+
+#DEFAULT    Service-Type == Framed-User, Huntgroup-Name == "delft"
+#        Framed-IP-Address = 192.168.2.32+,
+#        Fall-Through = Yes
+
+#
+# Sample defaults for all framed connections.
+#
+#DEFAULT    Service-Type == Framed-User
+#    Framed-IP-Address = 255.255.255.254,
+#    Framed-MTU = 576,
+#    Service-Type = Framed-User,
+#    Fall-Through = Yes
+
+#
+# Default for PPP: dynamic IP address, PPP mode, VJ-compression.
+# NOTE: we do not use Hint = "PPP", since PPP might also be auto-detected
+#    by the terminal server in which case there may not be a "P" suffix.
+#    The terminal server sends "Framed-Protocol = PPP" for auto PPP.
+#
+DEFAULT    Framed-Protocol == PPP
+    Framed-Protocol = PPP,
+    Framed-Compression = Van-Jacobson-TCP-IP
+
+#
+# Default for CSLIP: dynamic IP address, SLIP mode, VJ-compression.
+#
+DEFAULT    Hint == "CSLIP"
+    Framed-Protocol = SLIP,
+    Framed-Compression = Van-Jacobson-TCP-IP
+
+#
+# Default for SLIP: dynamic IP address, SLIP mode.
+#
+DEFAULT    Hint == "SLIP"
+    Framed-Protocol = SLIP
+
+#
+# Last default: rlogin to our main server.
+#
+#DEFAULT
+#    Service-Type = Login-User,
+#    Login-Service = Rlogin,
+#    Login-IP-Host = shellbox.ispdomain.com
+
+# #
+# # Last default: shell on the local terminal server.
+# #
+# DEFAULT
+#     Service-Type = Administrative-User
+
+# On no match, the user is denied access.
+"""
+
+FILEOUTPUT_USER_ORIG = """# File parsed and saved by privacyidea.
+
+DEFAULT Framed-Protocol == PPP
+\tFramed-Protocol = PPP,
+\tFramed-Compression = Van-Jacobson-TCP-IP
+
+DEFAULT Hint == "CSLIP"
+\tFramed-Protocol = SLIP,
+\tFramed-Compression = Van-Jacobson-TCP-IP
+
+DEFAULT Hint == "SLIP"
+\tFramed-Protocol = SLIP
+
+"""
+
 
 class TestFreeRADIUSParser(unittest.TestCase):
 
@@ -212,5 +456,98 @@ class TestFreeRADIUSParser(unittest.TestCase):
         self.assertEqual(output, FILEOUTPUT_ORIG)
         
 
+class TestFreeRADIUSUsers(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def test_users_basic(self):
+        UP = UserConfParser(content=USER_CONF_A)
+        config = UP.get()
+        print config
+        self.assertEqual(config[0][0], "DEFAULT")
+        self.assertEqual(config[0][1], "Auth-Type")
+        self.assertEqual(config[0][2], ":=")
+        self.assertEqual(config[0][3], "perl")
+        
+    def test_users_password(self):
+        UP = UserConfParser(content=USER_CONF_B)
+        config = UP.get()
+        print config
+        self.assertEqual(config[0][0], "administrator")
+        self.assertEqual(config[0][1], "Cleartext-Password")
+        self.assertEqual(config[0][2], ":=")
+        self.assertEqual(config[0][3], '"secret"')
+        
+    def test_operators(self):
+        UP = UserConfParser(content=USER_CONF_C)
+        config = UP.get()
+        print config
+        self.assertEqual(config[0][0], "user")
+        self.assertEqual(config[0][1], "somekey")
+        self.assertEqual(config[0][2], "==")
+        self.assertEqual(config[0][3], 'value')
+
+    def test_reply_items(self):
+        UP = UserConfParser(content=USER_CONF_D)
+        config = UP.get()
+        print config
+        # [['DEFAULT', 'Hint', '==', '"SLIP"',
+        #  [['Framed-Protocol', '=', 'SLIP']]]
+        # ]
+
+        self.assertEqual(config[0][0], "DEFAULT")
+        self.assertEqual(config[0][1], "Hint")
+        self.assertEqual(config[0][2], "==")
+        self.assertEqual(config[0][3], '"SLIP"')
+        self.assertEqual(config[0][4][0][0], 'Framed-Protocol')
+        self.assertEqual(config[0][4][0][1], '=')
+        self.assertEqual(config[0][4][0][2], 'SLIP')
+        
+    def test_reply_items2(self):
+        UP = UserConfParser(content=USER_CONF_E)
+        config = UP.get()
+        print config
+        # [['DEFAULT', 'Hint', '==', '"SLIP"',
+        #  [['Framed-Protocol', '=', 'SLIP']]]
+        # ]
+
+        self.assertEqual(config[0][0], "DEFAULT")
+        self.assertEqual(config[0][1], "Hint")
+        self.assertEqual(config[0][2], "==")
+        self.assertEqual(config[0][3], '"SLIP"')
+        self.assertEqual(config[0][4][0][0], 'Framed-Protocol')
+        self.assertEqual(config[0][4][0][1], '=')
+        self.assertEqual(config[0][4][0][2], 'SLIP')
+        self.assertEqual(config[0][4][1][0], 'Something')
+        self.assertEqual(config[0][4][1][1], '=')
+        self.assertEqual(config[0][4][1][2], 'Else')
+    
+    def test_get_complete(self):
+        UP = UserConfParser(content=USER_CONF_1)
+        config = UP.get()
+        print len(config)
+        self.assertEqual(len(config), 3)
+        print config[0]
+        print config[1]
+        print config[2]
+        self.assertEqual(len(config[0][4]), 2)
+        self.assertEqual(len(config[1][4]), 2)
+        self.assertEqual(len(config[2][4]), 1)
+        
+    def test_save_file(self):
+        tmpfile = "./tmp-output"
+        UP = UserConfParser(content=USER_CONF_1)
+        config = UP.get()
+        print config
+        UP.save(config, tmpfile)
+        f = open(tmpfile, "r")
+        output = f.read()
+        f.close()
+        os.unlink(tmpfile)
+        print output
+
+        self.assertEqual(output, FILEOUTPUT_USER_ORIG)
+    
 if __name__ == '__main__':
     unittest.main()
