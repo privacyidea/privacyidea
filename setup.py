8285d9111e41d4982d28d568b5da2f5c7277710b
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2015-05-22 10:13:57 +0200

    Add changelog and increase version to 2.3dev2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 import sys
 
 #VERSION="2.1dev4"
-VERSION="2.3dev1"
+VERSION="2.3dev2"
 
 # Taken from kennethreitz/requests/setup.py
 package_directory = os.path.realpath(os.path.dirname(__file__))
