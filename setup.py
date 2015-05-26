7a91a4847c9eac47dd141b7df6d3cba1d4fe4c7e
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2015-05-26 17:53:08 +0200

    add ldap3 versions

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
                       "docutils==0.12",
                       "funcparserlib==0.3.6",
                       "itsdangerous==0.24",
-                      "ldap3==0.9.8.1",
+                      "ldap3==0.9.8.4",
                       "netaddr==0.7.12",
                       "passlib==1.6.2",
                       "pyasn1==0.1.7",
