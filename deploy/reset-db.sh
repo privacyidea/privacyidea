5837d8538420debee6c415c6c5accdd6b1f23771
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2015-05-31 10:02:39 +0200

    Add User Enroll UI Test to circle CI

--- deploy/reset-db.sh
+++ deploy/reset-db.sh
@@ -2,3 +2,5 @@
 heroku pg:reset DATABASE_URL --app privacyidea-test
 heroku run python ./pi-manage.py createdb --app privacyidea-test
 heroku run --app privacyidea-test --  python ./pi-manage.py admin add -p 'Test1234!' admin admin@localhost
+heroku run --app privacyidea-test -- python ./pi-manage.py resolver create resolver1 passwdresolver deploy/heroku/default-resolver
+heroku run --app privacyidea-test -- python ./pi-manage.py realm create default resolver1
