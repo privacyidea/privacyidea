22148415ed358f9d6f8e32b5f023cbda2759fa16
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2016-03-28 23:54:17 +0200

    Set version 2.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 import sys
 
 #VERSION="2.1dev4"
-VERSION="2.11dev2"
+VERSION="2.11"
 
 # Taken from kennethreitz/requests/setup.py
 package_directory = os.path.realpath(os.path.dirname(__file__))
