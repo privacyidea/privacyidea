381f7f110eac24c957f89923b993ffd09e6ceac9
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2016-11-17 14:32:51 +0100

    Set token realm
    working on #532

--- privacyidea/lib/eventhandler/tokenhandler.py
+++ privacyidea/lib/eventhandler/tokenhandler.py
@@ -66,6 +66,13 @@ class NOTIFY_TYPE(object):
     EMAIL = "email"
 
 
+class ACTION_TYPE(object):
+    """
+    Allowed actions
+    """
+    SET_TOKENREALM = "set tokenrealm"
+
+
 class TokenEventHandler(BaseEventHandler):
     """
     An Eventhandler needs to return a list of actions, which it can handle.
@@ -92,7 +99,7 @@ class TokenEventHandler(BaseEventHandler):
         smsgateways = [sms.identifier for sms in smsgateway_dicts]
         smtpservers = [s.config.identifier for s in smtpserver_objs]
         from privacyidea.lib.token import get_token_types
-        actions = {"set tokenrealm":
+        actions = {ACTION_TYPE.SET_TOKENREALM:
                        {"realm":
                             {"type": "str",
                              "required": True,
@@ -140,144 +147,21 @@ class TokenEventHandler(BaseEventHandler):
         content = json.loads(response.data)
         handler_def = options.get("handler_def")
         handler_options = handler_def.get("options", {})
-        notify_type = handler_options.get("To", NOTIFY_TYPE.TOKENOWNER)
-        try:
-            logged_in_user = g.logged_in_user
-        except Exception:
-            logged_in_user = {}
 
-        tokenowner = self._get_tokenowner(request)
-        log.debug("Executing event for action {0!s}, user {1!s},"
-                  "logged_in_user {2!s}".format(action, tokenowner,
-                                                logged_in_user))
+        serial = request.all_data.get("serial") or \
+                 content.get("detail", {}).get("serial") or \
+                 g.audit_object.audit_data.get("serial")
 
-        # Determine recipient
-        recipient = None
-
-        if notify_type == NOTIFY_TYPE.TOKENOWNER and not tokenowner.is_empty():
-            recipient = {
-                "givenname": tokenowner.info.get("givenname"),
-                "surname": tokenowner.info.get("surname"),
-                "username": tokenowner.login,
-                "userrealm": tokenowner.realm,
-                "email": tokenowner.info.get("email"),
-                "mobile": tokenowner.info.get("mobile")
-            }
-        elif notify_type == NOTIFY_TYPE.INTERNAL_ADMIN:
-            username = handler_options.get("To "+NOTIFY_TYPE.INTERNAL_ADMIN)
-            internal_admin = get_db_admin(username)
-            recipient = {
-                "givenname": username,
-                "email": internal_admin.email if internal_admin else ""
-            }
-        elif notify_type == NOTIFY_TYPE.ADMIN_REALM:
-            # Send emails to all the users in the specified admin realm
-            admin_realm = handler_options.get("To "+NOTIFY_TYPE.ADMIN_REALM)
-            ulist = get_user_list({"realm": admin_realm})
-            # create a list of all user-emails, if the user has an email
-            emails = [u.get("email") for u in ulist if u.get("email")]
-            recipient = {
-                "givenname": "admin of realm {0!s}".format(admin_realm),
-                "email": emails
-            }
-        elif notify_type == NOTIFY_TYPE.LOGGED_IN_USER:
-            # Send notification to the logged in user
-            if logged_in_user.get("user") and not logged_in_user.get("realm"):
-                # internal admins have no realm
-                internal_admin = get_db_admin(logged_in_user.get("user"))
-                if internal_admin:
-                    recipient = {
-                        "givenname": logged_in_user.get("user"),
-                        "email": internal_admin.email if internal_admin else ""
-                    }
-            else:
-                # Try to find the user in the specified realm
-                user_obj = User(logged_in_user.get("user"),
-                                logged_in_user.get("realm"))
-                if user_obj:
-                    recipient = {
-                        "givenname": user_obj.info.get("givenname"),
-                        "surname": user_obj.info.get("surname"),
-                        "email": user_obj.info.get("email"),
-                        "mobile": user_obj.info.get("mobile")
-                    }
-
-        elif notify_type == NOTIFY_TYPE.EMAIL:
-            email = handler_options.get("To "+NOTIFY_TYPE.EMAIL, "").split(",")
-            recipient = {
-                "email": email
-            }
-        else:
-            log.warning("Was not able to determine the recipient for the user "
-                        "notification: {0!s}".format(handler_def))
-
-        if recipient:
-            # Collect all data
-            body = handler_options.get("body") or DEFAULT_BODY
-            serial = request.all_data.get("serial") or \
-                     content.get("detail", {}).get("serial") or \
-                     g.audit_object.audit_data.get("serial")
-            registrationcode = content.get("detail", {}).get("registrationcode")
-            tokentype = None
-            if serial:
-                tokens = get_tokens(serial=serial)
-                if tokens:
-                    tokentype = tokens[0].get_tokentype()
-            else:
-                token_objects = get_tokens(user=tokenowner)
-                serial = ','.join([tok.get_serial() for tok in token_objects])
-
-            body = body.format(
-                admin=logged_in_user.get("username"),
-                realm=logged_in_user.get("realm"),
-                action=request.path,
-                serial=serial,
-                url=request.url_root,
-                user=tokenowner.info.get("givenname"),
-                surname=tokenowner.info.get("surname"),
-                givenname=recipient.get("givenname"),
-                username=tokenowner.login,
-                userrealm=tokenowner.realm,
-                tokentype=tokentype,
-                registrationcode=registrationcode,
-                recipient_givenname=recipient.get("givenname"),
-                recipient_surname=recipient.get("surname")
-            )
-
-            # Send notification
-            if action.lower() == "sendmail":
-                emailconfig = handler_options.get("emailconfig")
-                useremail = recipient.get("email")
-                subject = handler_options.get("subject") or \
-                          "An action was performed on your token."
-                try:
-                    ret = send_email_identifier(emailconfig,
-                                                recipient=useremail,
-                                                subject=subject, body=body)
-                except Exception as exx:
-                    log.error("Failed to send email: {0!s}".format(exx))
-                    ret = False
-                if ret:
-                    log.info("Sent a notification email to user {0}".format(
-                        recipient))
-                else:
-                    log.warning("Failed to send a notification email to user "
-                                "{0}".format(recipient))
-
-            elif action.lower() == "sendsms":
-                smsconfig = handler_options.get("smsconfig")
-                userphone = recipient.get("mobile")
-                try:
-                    ret = send_sms_identifier(smsconfig, userphone, body)
-                except Exception as exx:
-                    log.error("Failed to send sms: {0!s}".format(exx))
-                    ret = False
-                if ret:
-                    log.info("Sent a notification sms to user {0}".format(
-                        recipient))
-                else:
-                    log.warning("Failed to send a notification email to user "
-                                "{0}".format(recipient))
+        if serial:
+            # DO THE ACTION
+            if action.lower() == ACTION_TYPE.SET_TOKENREALM:
+                realm = handler_options.get("realm")
+                only_realm = handler_options.get("only_realm")
+                # Set the realm..
+                log.info("Setting realm of token {0!s} to {1!s}".format(
+                    serial, realm))
+                from privacyidea.lib.token import set_realms
+                set_realms(serial, [realm])
 
         return ret
 
