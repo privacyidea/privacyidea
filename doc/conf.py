4c801321c754593a01106a9684115e772c2fe21d
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2017-03-02 20:53:58 +0100

    resolve import conflict

--- doc/conf.py
+++ doc/conf.py
@@ -11,6 +11,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 import sys
+import os
 from mock import Mock as MagicMock
 
 class Mock(MagicMock):
@@ -39,9 +40,6 @@ def no_op_wraps(func):
     return wrapper
 
 functools.wraps = no_op_wraps
-
-
-import sys, os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
