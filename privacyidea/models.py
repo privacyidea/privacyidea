03da416894364e13d2ececcb367790de07206071
Salvo Rapisarda <sdrapisarda@gmail.com>
2017-06-22 10:23:34 +0200

    Fix serial length on Audit model

--- privacyidea/models.py
+++ privacyidea/models.py
@@ -2226,7 +2226,7 @@ class Subscription(MethodsMixin, db.Model):
 
 audit_column_length = {"signature": 620,
                        "action": 50,
-                       "serial": 20,
+                       "serial": 40,
                        "token_type": 12,
                        "user": 20,
                        "realm": 20,
