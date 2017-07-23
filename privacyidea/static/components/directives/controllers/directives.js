560ec835c38beea32f886b1459e1a45159984d42
Friedrich Weber <friedrich.weber@netknights.it>
2017-07-23 12:25:36 +0200

    Add a button that refreshes the current route
    Working on #715

--- privacyidea/static/components/directives/controllers/directives.js
+++ privacyidea/static/components/directives/controllers/directives.js
@@ -357,7 +357,7 @@ myApp.directive('spinner', function() {
         },
         template: [
             '<div ng-show="show">',
-            '<span class="glyphicon glyphicon-refresh spin" style="margin: 50% 5px 0 0; font-size:120%; color: #787878;" aria-hidden="true"></span>',
+            '<span class="glyphicon glyphicon-refresh spin" style="margin: 0 5px 0 0; font-size:120%; color: #787878; padding: 15px 0px;" aria-hidden="true"></span>',
             '</div>'
         ].join(''),
         controller: function($scope) {
