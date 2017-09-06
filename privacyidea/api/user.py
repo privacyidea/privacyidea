f88ec2eb8f96f9c5ce793509c0146c4d448061cd
Friedrich Weber <friedrich.weber@netknights.it>
2017-09-06 10:05:01 +0200

    Fix creating/updating/deleting unicode users
    Working on #754

--- privacyidea/api/user.py
+++ privacyidea/api/user.py
@@ -130,7 +130,7 @@ def delete_user(resolvername=None, username=None):
     user_obj = User(login=username, resolver=resolvername)
     res = user_obj.delete()
     g.audit_object.log({"success": res,
-                        "info": "{0!s}".format(user_obj)})
+                        "info": u"{0!s}".format(user_obj)})
     return send_result(res)
 
 
@@ -172,7 +172,7 @@ def create_user_api():
     del attributes["password"]
     r = create_user(resolvername, attributes, password=password)
     g.audit_object.log({"success": True,
-                        "info": "{0!s}: {1!s}/{2!s}".format(r, username,
+                        "info": u"{0!s}: {1!s}/{2!s}".format(r, username,
                                                             resolvername)})
     return send_result(r)
 
@@ -220,7 +220,7 @@ def update_user():
         del attributes["password"]
     r = user_obj.update_user_info(attributes, password=password)
     g.audit_object.log({"success": True,
-                        "info": "{0!s}: {1!s}/{2!s}".format(r, username, resolvername)})
+                        "info": u"{0!s}: {1!s}/{2!s}".format(r, username, resolvername)})
     return send_result(r)
 
 
--- tests/test_api_users.py
+++ tests/test_api_users.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from .base import MyTestCase
 import json
 from privacyidea.lib.resolver import (save_resolver)
@@ -203,6 +204,117 @@ class APIUsersTestCase(MyTestCase):
 
         # Delete the users
         with self.app.test_request_context('/user/{0!s}/{1!s}'.format(resolver, "wordy"),
+                                           method='DELETE',
+                                           headers={'Authorization': self.at}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 200, res)
+            result = json.loads(res.data).get("result")
+            self.assertTrue(result.get("value"))
+
+    def test_03_create_update_delete_unicode_user(self):
+        realm = "sqlrealm"
+        resolver = "SQL1"
+        parameters = self.parameters
+        parameters["resolver"] = resolver
+        parameters["type"] = "sqlresolver"
+
+        rid = save_resolver(parameters)
+        self.assertTrue(rid > 0, rid)
+
+        (added, failed) = set_realm(realm, [resolver])
+        self.assertEqual(len(failed), 0)
+        self.assertEqual(len(added), 1)
+
+        # CREATE a user
+        with self.app.test_request_context('/user/',
+                                           method='POST',
+                                           data={"user": u"wördy",
+                                                 "resolver": resolver,
+                                                 "surname": "zappa",
+                                                 "givenname": "frank",
+                                                 "email": "f@z.com",
+                                                 "phone": "12345",
+                                                 "mobile": "12345",
+                                                 "password": "12345"},
+                                           headers={'Authorization': self.at}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 200, res)
+            result = json.loads(res.data).get("result")
+            self.assertTrue(result.get("value") > 6, result.get("value"))
+
+        # Get users
+        with self.app.test_request_context('/user/',
+                                           method='GET',
+                                           query_string=urlencode(
+                                               {"username": u"wördy".encode('utf-8')}),
+                                           headers={'Authorization': self.at}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 200, res)
+            result = json.loads(res.data).get("result")
+            self.assertTrue(result.get("status"))
+            self.assertEqual(result.get("value")[0].get("username"), u"wördy")
+
+        # Update by administrator. Set the password to "passwort"
+        with self.app.test_request_context('/user/',
+                                           method='PUT',
+                                           query_string=urlencode(
+                                               {"user": u"wördy".encode('utf-8'),
+                                                "resolver": resolver,
+                                                "password": "passwort"}),
+                                           headers={'Authorization': self.at}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 200, res)
+            result = json.loads(res.data).get("result")
+            self.assertTrue(result.get("value"))
+
+        # Get user authentication and update by user.
+        with self.app.test_request_context('/auth',
+                                           method='POST',
+                                           data={"username": u"wördy@{0!s}".format(realm).encode('utf-8'),
+                                                 "password": "passwort"}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 200, res)
+            result = json.loads(res.data).get("result")
+            self.assertTrue(result.get("status"), res.data)
+            # In self.at_user we store the user token
+            wordy_auth_token = result.get("value").get("token")
+            # check that this is a user
+            role = result.get("value").get("role")
+            self.assertTrue(role == "user", result)
+
+        # Even if a user specifies another username, the username is
+        # overwritten by his own name!
+        with self.app.test_request_context('/user/',
+                                           method='PUT',
+                                           query_string=urlencode(
+                                               {"user": u"wördy2".encode('utf-8'),
+                                                "resolver": resolver,
+                                                "password": "newPassword"}),
+                                           headers={'Authorization':
+                                                        wordy_auth_token}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 200, res)
+            result = json.loads(res.data).get("result")
+            self.assertTrue(result.get("value"))
+
+        # Although the user "wördy" tried to update the password of user
+        # "wördy2", he updated his own password.
+        with self.app.test_request_context('/auth',
+                                           method='POST',
+                                           data={"username": u"wördy@{0!s}".format(realm).encode('utf-8'),
+                                                 "password": "newPassword"}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 200, res)
+            result = json.loads(res.data).get("result")
+            self.assertTrue(result.get("status"), res.data)
+            # In self.at_user we store the user token
+            wordy_auth_token = result.get("value").get("token")
+            # check that this is a user
+            role = result.get("value").get("role")
+            self.assertTrue(role == "user", result)
+
+        # Delete the users
+        with self.app.test_request_context(u'/user/{0!s}/{1!s}'.format(resolver, u"wördy").encode('utf-8'),
                                            method='DELETE',
                                            headers={'Authorization': self.at}):
             res = self.app.full_dispatch_request()
