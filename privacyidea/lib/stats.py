74bf512b2ecfffb398d42b806e73b3f3ac92dfb0
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2017-11-24 08:31:16 +0100

    Suppress the logger output privacyidea.lib.stats
    The error
    'No handlers could be found for logger "privacyidea.lib.stats"'
    was due to the logging attempt outside of the application
    context when the module was loaded. This way we suppress
    the output in the command line tools.

--- privacyidea/lib/stats.py
+++ privacyidea/lib/stats.py
@@ -36,8 +36,8 @@ try:
     matplotlib.use('Agg')
 except Exception as exx:
     MATPLOT_READY = False
-    log.warning("If you want to see statistics you need to install python "
-                "matplotlib.")
+    #log.warning("If you want to see statistics you need to install python "
+    #            "matplotlib.")
 
 customcmap = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
 
@@ -74,6 +74,7 @@ def get_statistics(auditobject, start_time=datetime.datetime.now()
 
     return result
 
+
 def _get_success_fail(df, key):
 
     try:
@@ -95,6 +96,7 @@ def _get_success_fail(df, key):
         log.info(exx)
         image_uri = "{0!s}".format(exx)
     return image_uri
+
 
 def _get_fail(df, key):
 
@@ -123,7 +125,6 @@ def _get_fail(df, key):
         log.info(exx)
         image_uri = "{0!s}".format(exx)
     return image_uri
-
 
 
 def _get_number_of(df, key, nums=5):
