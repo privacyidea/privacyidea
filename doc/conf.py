198805ebec1c98ab8643ad762d60030c6b15d76e
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2018-01-12 12:02:00 +0100

    Set version to 2.22 dev0

--- doc/conf.py
+++ doc/conf.py
@@ -17,10 +17,10 @@
 # built documents.
 #
 # The short X.Y version.
-version = '2.21'
+version = '2.22dev0'
 # The full version, including alpha/beta/rc tags.
 #release = '2.16dev5'
-release = '2.21'
+release = version
 
 
 import sys
