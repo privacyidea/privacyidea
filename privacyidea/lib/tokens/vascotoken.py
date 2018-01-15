e82c7714a192dd4adfce050d9cb5a61dcbb6ddf2
Friedrich Weber <friedrich.weber@netknights.it>
2018-01-15 14:05:25 +0100

    Add PoC of VASCO token
    Working on #891

---
+++ privacyidea/lib/tokens/vascotoken.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+#
+#  License:  AGPLv3
+#  contact:  http://www.privacyidea.org
+#
+#  2018-01-15   Friedrich Weber <friedrich.weber@netknights.it>
+#               Initial version of the VASCO token
+#
+# This code is free software; you can redistribute it and/or
+# modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or any later version.
+#
+# This code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+__doc__ = """This is the implementation of the VASCO token"""
+
+import logging
+from privacyidea.api.lib.utils import getParam
+from privacyidea.lib.utils import is_true
+from privacyidea.lib.decorators import check_token_locked
+from privacyidea.lib.error import ParameterError
+from privacyidea.lib.log import log_with
+from privacyidea.lib.tokenclass import TokenClass
+from privacyidea.lib import _
+
+optional = True
+required = False
+
+log = logging.getLogger(__name__)
+
+
+class VascoTokenClass(TokenClass):
+    """
+    TODO
+    """
+
+    def __init__(self, db_token):
+        """
+        constructor - create a token class object with its db token binding
+
+        :param aToken: the db bound token
+        """
+        TokenClass.__init__(self, db_token)
+        self.set_type(u"vasco")
+        self.hKeyRequired = True
+
+    @staticmethod
+    def get_class_type():
+        """
+        return the class type identifier
+        """
+        return "vasco"
+
+    @staticmethod
+    def get_class_prefix():
+        """
+        return the token type prefix
+        """
+        return "VASC"
+
+    @staticmethod
+    @log_with(log)
+    def get_class_info(key=None, ret='all'):
+        """
+        :param key: subsection identifier
+        :type key: string
+        :param ret: default return value, if nothing is found
+        :type ret: user defined
+        :return: subsection if key exists or user defined
+        :rtype: dict or string
+        """
+        res = {'type': 'vasco',
+               'title': 'VASCO Token',
+               'description': _('VASCO Token: Authentication using VASCO tokens'),
+               'user': ["enroll"],
+               'ui_enroll': ["admin", "user"],
+               'policy': {},
+               }
+
+        if key:
+            ret = res.get(key, {})
+        else:
+            if ret == 'all':
+                ret = res
+        return ret
+
+    @log_with(log)
+    def update(self, param, reset_failcount=True):
+        """
+        update - process initialization parameters
+
+        :param param: dict of initialization parameters
+        :type param: dict
+
+        :return: nothing
+        """
+        if is_true(getParam(param, 'genkey', optional)):
+            raise ParameterError("Generating OTP keys is not supported")
+
+        TokenClass.update(self, param, reset_failcount)
+
+        return
+
+    @check_token_locked
+    def check_otp(self, otpval, counter=None, window=None, options=None):
+        from .vasco import vasco_otp_check
+
+        secret = self.token.get_otpkey().getKey()
+        result, new_secret = vasco_otp_check(secret, otpval)
+        self.token.set_otpkey(new_secret)
+
+        if result == 0:
+            # Successful authentication
+            return 0
+        else:
+            log.warning("VASCO token failed to authenticate, result: {!r}".format(result))
+            return -1
---
+++ tests/test_lib_tokens_vasco.py
@@ -0,0 +1,102 @@
+"""
+This test file tests the lib.tokens.vascotoken
+This depends on lib.tokenclass
+"""
+import functools
+
+import mock
+
+from privacyidea.lib.error import ParameterError
+from privacyidea.lib.tokens.vascotoken import VascoTokenClass
+from privacyidea.models import Token
+from tests.base import MyTestCase
+
+def mock_verification(replacement):
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            with mock.patch('privacyidea.lib.tokens.vasco.vasco_dll') as mock_dll:
+                mock_dll.AAL2VerifyPassword.side_effect = replacement
+                return f(*args, **kwargs)
+        return wrapper
+    return decorator
+
+def mock_success(data, params, password, challenge):
+    # fake a new blob
+    data._obj.Blob = "Y" * 224
+    return 0
+
+def mock_failure(data, params, password, challenge):
+    # fake a new blob
+    data._obj.Blob = "Y"*224
+    return 42
+
+
+class VascoTokenTest(MyTestCase):
+    otppin = "topsecret"
+    serial1 = "ser1"
+    serial2 = "ser2"
+    resolvername1 = "resolver1"
+    resolvername2 = "Resolver2"
+    resolvername3 = "reso3"
+    realm1 = "realm1"
+    realm2 = "realm2"
+
+    def test_01_create_token(self):
+        db_token = Token(self.serial1, tokentype="vasco")
+        db_token.save()
+        token = VascoTokenClass(db_token)
+        token.update({"otpkey": "X" * 248,
+                      "pin": self.otppin})
+        self.assertTrue(token.token.serial == self.serial1, token)
+        self.assertTrue(token.token.tokentype == "vasco", token.token.tokentype)
+        self.assertTrue(token.type == "vasco", token)
+        class_prefix = token.get_class_prefix()
+        self.assertTrue(class_prefix == "VASC", class_prefix)
+        self.assertTrue(token.get_class_type() == "vasco", token)
+
+    def test_02_genkey_fails(self):
+        db_token = Token(self.serial2, tokentype="vasco")
+        db_token.save()
+        token = VascoTokenClass(db_token)
+        self.assertRaises(ParameterError, token.update, {"genkey": "1"})
+        token.delete_token()
+
+    def test_03_no_vasco_library(self):
+        db_token = Token(self.serial2, tokentype="vasco")
+        db_token.save()
+        token = VascoTokenClass(db_token)
+        token.update({"otpkey": "X"*248,
+                      "pin": self.otppin})
+        self.assertRaises(RuntimeError, token.authenticate, "{}123456".format(self.otppin))
+        token.delete_token()
+
+    @mock_verification(mock_failure)
+    def test_04_failure(self):
+        db_token = Token(self.serial2, tokentype="vasco")
+        db_token.save()
+        token = VascoTokenClass(db_token)
+        token.update({"otpkey": "X"*248,
+                      "pin": self.otppin})
+        r = token.authenticate("{}123456".format(self.otppin))
+        self.assertEqual(r[0], True)
+        self.assertEqual(r[1], -1)
+        # failure, but the token secret has been updated nonetheless
+        key = token.token.get_otpkey().getKey()
+        self.assertEqual(key, "X"*24 + "Y"*224)
+        token.delete_token()
+
+    @mock_verification(mock_success)
+    def test_05_success(self):
+        db_token = Token(self.serial2, tokentype="vasco")
+        db_token.save()
+        token = VascoTokenClass(db_token)
+        token.update({"otpkey": "X"*248,
+                      "pin": self.otppin})
+        r = token.authenticate("{}123456".format(self.otppin))
+        self.assertEqual(r[0], True)
+        self.assertEqual(r[1], 0) # TODO: that is success?
+        # failure, but the token secret has been updated nonetheless
+        key = token.token.get_otpkey().getKey()
+        self.assertEqual(key, "X"*24 + "Y"*224)
+        token.delete_token()
