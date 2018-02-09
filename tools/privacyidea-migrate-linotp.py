b41fd31c23832f65b1f8bdffd5d486e553503fd7
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2018-02-09 14:56:24 +0100

    Speed up linotp database migration
    Closes #914

--- tools/privacyidea-migrate-linotp.py
+++ tools/privacyidea-migrate-linotp.py
@@ -1,16 +1,63 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
+#  2018-02-09 Cornelius Kölbel <cornelius.koelbel@netknights.it>
+#             Speedup thanks to Friedrich Weber.
+#             Add flexible resolver mapping
 #  2016-09-14 Cornelius Kölbel <cornelius.koelbel@netknights.it>
 #
 """
 You can use this script to migrate a LinOTP database to privacyIDEA.
 All tokens and token assignments are transferred to privacyIDEA.
 
+You only need an export of the LinOTP "Token" table.
+Please configure below, how your database URIs look like,
+what should be migrated and what should be mapped.
 """
 # You need to change this!
-LINOTP_URI = "mysql://linotp2:testtest@localhost/LinOTP2"
-PRIVACYIDEA_URI = "mysql://pi:pi@localhost/pi"
+LINOTP_URI = "mysql://linotp2:linotp2@localhost/linotp2"
+PRIVACYIDEA_URI = "mysql://pitest:pitest@localhost/pitest"
+
+
+"""
+Define, what should be migrated:
+
+"tokens" should always be True.
+If "assignments" is False, the imported tokens will be unassigned!
+
+"""
+MIGRATE = {"tokens": True,
+           "tokeninfo": True,
+           "assignments": True}
+
+"""
+This assigns the token to the new resolver
+
+The "resolver" key maps LinOTP-resolvers to privacyIDEA-Resolvers. This value is
+rewritten in the token tabel.
+
+The "realm" key puts tokens in the privacyIDEA-Resolver into this privacyIDEA realm.
+
+In this example the resolvername "lokal" from LinOTP gets imported to
+privacyIDEA with the resolver "PIResolver" in realm "pirealm".
+"""
+ASSIGNMENTS = {
+    "resolver": {"lokal": "PIResolver"},
+    "realm": {"PIResolver": "pirealm"}
+}
+
+# Do not change anything after this line
+# ============================================================================
+#
+
+# THis maps the resolver types. You must not change this!
+resolver_map = {"LDAPIdResolver": "ldapresolver",
+                "SQLIdResolver": "sqlresolver",
+                "PasswdIdResolver": "passwdresolver"}
+
+token_serial_id_map = {}
+realm_id_map = {}
+resolver_id_map = {}
 
 from sqlalchemy import Table, MetaData, Column
 from sqlalchemy import Integer, Unicode, Boolean, UnicodeText
@@ -173,182 +220,135 @@ privacyidea_session = sessionmaker(bind=privacyidea_engine)()
 conn_linotp = linotp_engine.connect()
 conn_pi = privacyidea_engine.connect()
 
-resolver_map = {"LDAPIdResolver": "ldapresolver",
-                "SQLIdResolver": "sqlresolver",
-                "PasswdIdResolver": "passwdresolver"}
+
+# Values to be imported
+token_values = []
+tokeninfo_values = []
+tokenrealm_values = []
+
+# Process Assignments in table "tokenrealm"
+if MIGRATE.get("assignments"):
+    s = select([realm_table])
+    result = conn_pi.execute(s)
+    for r in result:
+        realm_id_map[r["name"]] = r["id"]
+
+    s = select([resolver_table])
+    result = conn_pi.execute(s)
+    for r in result:
+        resolver_id_map[r["name"]] = r["id"]
+
+    print("Realm-Map: {}".format(realm_id_map))
+    print("Resolver-Map: {}".format(resolver_id_map))
 
 # Process Tokens
 
-s = select([linotp_token_table])
-result = conn_linotp.execute(s)
-for r in result:
-    print("migrating token {0!s}".format(r["LinOtpTokenSerialnumber"]))
-    # Adapt type
-    type = r["LinOtpTokenType"]
-    if type.lower() == "hmac":
-        type = "HOTP"
-    # Adapt resolver
-    resolver = r["LinOtpIdResolver"].split(".")[-1]
+if MIGRATE.get("tokens"):
+    s = select([linotp_token_table])
+    result = conn_linotp.execute(s)
 
-    # Adapt resolver type
-    resolver_type = ""
-    if r["LinOtpIdResClass"]:
-        resolver_type = r['LinOtpIdResClass'].split(".")[1]
-        resolver_type = resolver_map.get(resolver_type)
-
-    # Adapt tokeninfo
-    ti = {}
-    if r["LinOtpTokenInfo"]:
-        ti = json.loads(r["LinOtpTokenInfo"])
-
-    ins = token_table.insert().values(
-        id=r["LinOtpTokenId"],
-        description=r["LinOtpTokenDesc"],
-        serial=r["LinOtpTokenSerialnumber"],
-        tokentype=type,
-        user_pin=r['LinOtpTokenPinUser'],
-        user_pin_iv=r['LinOtpTokenPinUserIV'],
-        so_pin=r['LinOtpTokenPinSO'],
-        so_pin_iv=r['LinOtpTokenPinSOIV'],
-        resolver=resolver,
-        resolver_type=resolver_type,
-        user_id=r['LinOtpUserid'],
-        pin_seed=r['LinOtpSeed'],
-        key_enc=r['LinOtpKeyEnc'],
-        key_iv=r['LinOtpKeyIV'],
-        maxfail=r['LinOtpMaxFail'],
-        active=r['LinOtpIsactive'],
-        failcount=r['LinOtpFailCount'],
-        count=r['LinOtpCount'],
-        count_window=r['LinOtpCountWindow'],
-        sync_window=r['LinOtpSyncWindow'])
-    conn_pi.execute(ins)
-
-    if ti:
-        # Add tokeninfo for this token
-        for k, v in ti.iteritems():
-            ins = tokeninfo_table.insert().values(
-                Key=k, Value=v, token_id=r["LinOtpTokenId"])
-            conn_pi.execute(ins)
-            print(" +--- adding tokeninfo {0!s}".format(k))
-
-# Process Realms
-
-s = select([linotp_realm_table])
-result = conn_linotp.execute(s)
-for r in result:
-    print("migrating realm {0!s}".format(r["name"]))
-    ins = realm_table.insert().values(
-        id=r["id"],
-        name=r["name"],
-        default=r["default"],
-        option=r["option"]
-    )
-    conn_pi.execute(ins)
-
-# Process Tokenrealms
-
-s = select([linotp_tokenrealm_table])
-result = conn_linotp.execute(s)
-for r in result:
-    print("migrating tokenrealm {0!s}".format(r["id"]))
-    ins = tokenrealm_table.insert().values(
-        id=r["id"],
-        token_id=r["token_id"],
-        realm_id=r["realm_id"]
-    )
-    conn_pi.execute(ins)
-
-# Process Resolvers
-
-s = select([linotp_config_table])
-result = conn_linotp.execute(s)
-resolvers = {}
-for r in result:
-    config_key = r["Key"]
-    # TODO: We need to supprt the other resolvers!
-    entry = config_key.split(".")
-    if entry[1] == "ldapresolver":
-        name = entry[3]
-        resolver_key = entry[2]
-        value = r["Value"]
-        desc = r["Description"]
-        type = r["Type"]
-
-        if name not in resolvers:
-            resolvers[name] = {"type": "ldapresolver",
-                               "config": [{"Key": "CACHE_TIMEOUT",
-                                           "Value": "120"},
-                                          {"Key": "SCOPE",
-                                           "Value": "SUBTREE",
-                                           "Type": "string"},
-                                          {"Key": "EDITABLE",
-                                           "Value": "0",
-                                           "Type": "bool"}]}
-        resolvers[name]["config"].append({
-            "Key": resolver_key,
-            "Value": value,
-            "Description": desc,
-            "Type": type
-        })
-
-if resolvers:
-    # Write the resolvers
-    for name, config in resolvers.iteritems():
-        print("Migrating Resolver {0!s}".format(name))
-
-        ins = resolver_table.insert().values(
-                name=name,
-                rtype=config.get("type")
-        )
-        res_id = conn_pi.execute(ins)
-        resolver_id = res_id.inserted_primary_key[0]
-        for resolver_entry in config.get("config"):
-            # save the list of resolver configuration
-            ins = resolver_config_table.insert().values(
-                resolver_id=resolver_id,
-                Key=resolver_entry.get("Key") or "",
-                Value=resolver_entry.get("Value") or "",
-                Type=resolver_entry.get("Type") or "",
-                Description=resolver_entry.get("Description") or ""
-            )
-            conn_pi.execute(ins)
-
-
-# Put resolvers into realms
-s = select([linotp_config_table])
-result = conn_linotp.execute(s)
-realms = {}
-for r in result:
-    config_key = r["Key"]
-    entry = config_key.split(".")
-    if entry[1] == "useridresolver" and entry[2] == "group":
-        resolvers = r["Value"].split()
-        realms[entry[3]] = [x.split(".")[3] for x in resolvers]
-
-print("Migrating realm definitions {0}".format(realms))
-
-# Save the resolvers in the realms
-"""For this we need to get the IDs of the realmname and the resolvernames"""
-for realmname, resolvers in realms.iteritems():
-    print("Migrating realm {0!s}".format(realmname))
-    s = select([realm_table]).where(realm_table.c.name == realmname)
-    result = conn_pi.execute(s)
+    i = 0
     for r in result:
-        realm_id = r["id"]
+        i = i + 1
+        print("processing token #{1!s}: {0!s}".format(r["LinOtpTokenSerialnumber"], i))
+        # Adapt type
+        type = r["LinOtpTokenType"]
+        if type.lower() == "hmac":
+            type = "HOTP"
+        # Adapt resolver
+        linotp_resolver = r["LinOtpIdResClass"].split(".")[-1]
 
-    for resolvername in resolvers:
-        s = select([resolver_table]).where(resolver_table.c.name ==
-                                           resolvername)
+        # Adapt resolver type
+        resolver_type = ""
+        if r["LinOtpIdResClass"]:
+            resolver_type = r['LinOtpIdResClass'].split(".")[1]
+            resolver_type = resolver_map.get(resolver_type)
+
+        # Adapt tokeninfo
+        ti = {}
+        if r["LinOtpTokenInfo"]:
+            ti = json.loads(r["LinOtpTokenInfo"])
+
+        if MIGRATE.get("assignments"):
+            user_pin = r['LinOtpTokenPinUser']
+            user_pin_iv = r['LinOtpTokenPinUserIV']
+            # Map the LinOTP-Resolver to the PI-Resolver
+            resolver = ASSIGNMENTS.get("resolver").get(linotp_resolver)
+            resolver_type = resolver_type
+            user_id = r['LinOtpUserid']
+        else:
+            user_pin = None
+            user_pin_iv = None
+            resolver = None
+            resolver_type = None
+            user_id = None
+
+        token_values.append(dict(
+            id=r["LinOtpTokenId"],
+            description=r["LinOtpTokenDesc"],
+            serial=r["LinOtpTokenSerialnumber"],
+            tokentype=type,
+            user_pin=user_pin,
+            user_pin_iv=user_pin_iv,
+            so_pin=r['LinOtpTokenPinSO'],
+            so_pin_iv=r['LinOtpTokenPinSOIV'],
+            resolver=resolver,
+            resolver_type=resolver_type,
+            user_id=user_id,
+            pin_seed=r['LinOtpSeed'],
+            key_enc=r['LinOtpKeyEnc'],
+            key_iv=r['LinOtpKeyIV'],
+            maxfail=r['LinOtpMaxFail'],
+            active=r['LinOtpIsactive'],
+            failcount=r['LinOtpFailCount'],
+            count=r['LinOtpCount'],
+            count_window=r['LinOtpCountWindow'],
+            sync_window=r['LinOtpSyncWindow']))
+
+        if MIGRATE.get("tokeninfo") and ti:
+            # Add tokeninfo for this token
+            for k, v in ti.iteritems():
+                tokeninfo_values.append(dict(
+                    serial=r["LinOtpTokenSerialnumber"],
+                    Key=k, Value=v,
+                    token_id=r["LinOtpTokenId"]))
+                print(" +--- processing tokeninfo {0!s}".format(k))
+
+    print
+    print("Adding {} tokens...".format(len(token_values)))
+    conn_pi.execute(token_table.insert(), token_values)
+
+    if MIGRATE.get("tokeninfo"):
+        # fet the new token_id's in privacyIDEA
+        s = select([token_table])
         result = conn_pi.execute(s)
         for r in result:
-            resolver_id = r["id"]
-        # insert each resolver_id for the realm_id
-        ins = resolverrealm_table.insert().values(
-            realm_id=realm_id,
-            resolver_id=resolver_id
-        )
-        print(" +--- Adding resolver {0!s} to realm {1!s}".format(
-            resolvername, realmname))
-        conn_pi.execute(ins)
+            token_serial_id_map[r["serial"]] = r["id"]
+
+        # Now we have to rewrite the token_id in the tokeninfo_values
+        for ti in tokeninfo_values:
+            ti["token_id"] = token_serial_id_map[ti["serial"]]
+            del ti["serial"]
+
+        print("Adding {} token infos...".format(len(tokeninfo_values)))
+        conn_pi.execute(tokeninfo_table.insert(), tokeninfo_values)
+
+if MIGRATE.get("assignments") and resolver:
+    # If the token is assigned, we also need to create an entry for tokenrealm
+    # We need to determine the realm_id for this resolver!
+    for token in token_values:
+        token_id = token.get("id")
+        resolver = token.get("resolver")
+        realm = ASSIGNMENTS.get("realm").get(resolver)
+        realm_id = realm_id_map.get(realm)
+        print("Assigning token {} for resolver {} to realm_id {} (realm {})").format(token_id,
+                                                                                     resolver,
+                                                                                     realm_id,
+                                                                                     realm)
+        tokenrealm_values.append(dict(token_id=token_id,
+                                      realm_id=realm_id))
+
+    print("Adding {} tokenrealms...".format(len(tokenrealm_values)))
+    conn_pi.execute(tokenrealm_table.insert(), tokenrealm_values)
+
 
