c741c8cc1a2f3bcd6e360fbbd773b433547ad6d6
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2018-05-27 11:55:45 +0200

    Allow to only update counters, that are bigger
    Working on #1075

--- tools/privacyidea-update-counter.py
+++ tools/privacyidea-update-counter.py
@@ -36,12 +36,13 @@ def read_counter_file(import_file):
     for line in import_file.readlines():
         try:
             serial, counter = [v.strip() for v in line.split(",")]
-            update_list.append((u"{0!s}".format(serial), counter))
+            update_list.append((u"{0!s}".format(serial), int(counter)))
         except ValueError as ve:
             # If there is a line, that does not comply
             sys.stderr.write("Failed to parse line: {0!s}".format(line))
 
     return update_list
+
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("-c", "--config", help="privacyIDEA config file. We only need the SQLALCHEMY_DATABASE_URI.",
@@ -49,6 +50,9 @@ parser.add_argument("-c", "--config", help="privacyIDEA config file. We only nee
 parser.add_argument('file', help='The CSV file with the updated counters. The file should contain one serial and '
                                  'counter per line split by a comma. You can specify "-" to read from stdin.',
                     type=argparse.FileType())
+parser.add_argument("-i", "--increase_only", help="Only update the token counter, if the new counter value"
+                                                  "is bigger than the existing in the database.",
+                    action='store_const', const=True)
 args = parser.parse_args()
 
 # Parse data
@@ -62,12 +66,19 @@ privacyidea_engine = create_engine(SQL_URI)
 privacyidea_session = sessionmaker(bind=privacyidea_engine)()
 
 print("Starting updating {0!s} counters:".format(len(counters)))
+updated = 0
 for count in counters:
-    sys.stdout.write(".")
+    if args.increase_only:
+        r = privacyidea_session.query(Token).filter_by(serial=count[0]).first()
+        if r.count >= count[1]:
+            # The counter in the database is bigger
+            continue
+    sys.stdout.write("{0!s}, ".format(count[0]))
     privacyidea_session.query(Token).filter_by(serial=count[0]).update({"count": count[1]})
+    updated += 1
     # Depending on the time of running, we might do the session.commit after each update to avoid
     # blocking the Token table.
 
 privacyidea_session.commit()
 
-print("\nDone: {0!s} counters updated.".format(len(counters)))
+print("\nDone: {0!s} counters updated.".format(updated))
