a089be25242d1af02a6b4f46d56723721ee5bff5
Friedrich Weber <friedrich.weber@netknights.it>
2018-06-25 09:30:16 +0200

    Add test for BaseTask

---
+++ tests/test_lib_tasks.py
@@ -0,0 +1,26 @@
+"""
+This file contains the tests for tasks.
+
+In particular, this tests
+lib/task/base.py
+"""
+
+from privacyidea.lib.task.base import BaseTask
+from .base import MyTestCase
+
+
+class BaseTaskTestCase(MyTestCase):
+    def test_01_base_functions(self):
+        task = BaseTask()
+
+        self.assertEqual(task.identifier, "BaseTask")
+        self.assertIsInstance(task.description, basestring)
+        self.assertEqual(task.options, {})
+
+        result1 = task.do()
+        self.assertTrue(result1)
+
+        result2 = task.do({
+            "foo": "bar"
+        })
+        self.assertTrue(result2)
