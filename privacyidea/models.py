6cc6e6ab12ae01c9920325c60363b01acd3ffc85
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2018-06-26 13:02:18 +0200

    Define the database table for statistics data
    Working on #1027

--- privacyidea/models.py
+++ privacyidea/models.py
@@ -2716,3 +2716,37 @@ class PeriodicTaskLastRun(db.Model):
             ret = last_run.id
         db.session.commit()
         return ret
+
+
+class MonitoringStats(MethodsMixin, db.Model):
+    """
+    This is the table that stores measured, arbitrary statistic points in time.
+
+    This could be used to store time series but also to store current values,
+    by simply fetching the last value from the database.
+    """
+    __tablename__ = 'monitoringstats'
+    id = db.Column(db.Integer, Sequence("monitoringstats_seq"),
+                   primary_key=True)
+    timestamp = db.Column(db.DateTime(False), nullable=False)
+    stats_key = db.Column(db.Unicode(128), nullable=False)
+    stats_value = db.Column(db.Integer, nullable=False, default=0)
+
+    __table_args__ = (db.UniqueConstraint('timestamp',
+                                          'stats_key',
+                                          name='msix_1'), {})
+
+    def __init__(self, timestamp, key, value):
+        """
+        Create a new database entry in the monitoring stats table
+        :param timestamp: The time of the measurement point
+        :type timestamp: datetime
+        :param key: The key of the measurement
+        :type key: basestring
+        :param value: The value of the measurement
+        :type value: Int
+        """
+        self.timestamp = timestamp
+        self.stats_key = key
+        self.stats_value = value
+        self.save()
