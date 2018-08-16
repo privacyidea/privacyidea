d79c1c88ef425f651d587e03e4cfbbba0f87b2de
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2018-08-16 21:52:21 +0200

    Replace deprecated sphinx module

--- doc/conf.py
+++ doc/conf.py
@@ -68,7 +68,7 @@ sys.path.insert(0, os.path.abspath('../privacyidea'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath', 'sphinx.ext.viewcode', 
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.imgmath', 'sphinx.ext.viewcode', 
               'sphinxcontrib.autohttp.flask']
 http_index_ignore_prefixes = ['/token']
 
