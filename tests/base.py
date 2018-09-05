521a4429313d810a3232342d527b22c0df2b8a52
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2018-09-05 23:50:18 +0200

    triggerchallenge only returns active tokens (#1219)
    * triggerchallenge only returns active tokens
    * Added review comments
      removing unnecessary lines.
    
    If a response to a challenge is sent, we also
    check if the token, that sends the response is still
    active.
    
    Fixes #1217

--- tests/base.py
+++ tests/base.py
@@ -42,7 +42,7 @@ class MyTestCase(unittest.TestCase):
                         "399871",
                         "520489"]
 
-
+    
     @classmethod
     def setUpClass(cls):
         cls.app = create_app('testing', "")
