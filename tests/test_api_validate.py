d1dbc8529e6ea32fb5fea9a666a71627698bdf9d
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2018-09-07 12:17:31 +0200

    Add tests for invalid usernames and serial
    Closes #1227

--- tests/test_api_validate.py
+++ tests/test_api_validate.py
@@ -2193,6 +2193,40 @@ class ValidateAPITestCase(MyTestCase):
         remove_token("CHAL3")
         remove_token("CHAL4")
 
+    def test_01_check_invalid_input(self):
+        # Empty username
+        with self.app.test_request_context('/validate/check',
+                                           method='POST',
+                                           data={"user": " ",
+                                                 "pass": ""}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 400, res)
+            result = json.loads(res.data).get("result")
+            error_msg = result.get("error").get("message")
+            self.assertEqual("ERR905: You need to specify a serial or a user.", error_msg)
+
+        # wrong username
+        with self.app.test_request_context('/validate/check',
+                                           method='POST',
+                                           data={"user": "h%h",
+                                                 "pass": ""}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 400, res)
+            result = json.loads(res.data).get("result")
+            error_msg = result.get("error").get("message")
+            self.assertEqual("ERR905: Invalid user.", error_msg)
+
+        # wrong serial
+        with self.app.test_request_context('/validate/check',
+                                           method='POST',
+                                           data={"serial": "*",
+                                                 "pass": ""}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 400, res)
+            result = json.loads(res.data).get("result")
+            error_msg = result.get("error").get("message")
+            self.assertEqual("ERR905: Invalid serial number.", error_msg)
+
 
 class AChallengeResponse(MyTestCase):
 
