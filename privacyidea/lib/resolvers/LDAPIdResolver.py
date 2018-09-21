558080cd2e844d9e58d03c4f29fbd47b69dc5099
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2018-09-21 22:10:14 +0200

    Cleap up LDAP cache
    Delete expired entries
    
    Closes #1246

--- privacyidea/lib/resolvers/LDAPIdResolver.py
+++ privacyidea/lib/resolvers/LDAPIdResolver.py
@@ -190,17 +190,25 @@ def cache(func):
     def cache_wrapper(self, *args, **kwds):
         # If it does not exist, create the node for this instance
         resolver_id = self.getResolverId()
+        now = datetime.datetime.now()
+        tdelta = datetime.timedelta(seconds=self.cache_timeout)
         if not resolver_id in CACHE:
             CACHE[resolver_id] = {"getUserId": {},
                                   "getUserInfo": {},
                                   "_getDN": {}}
+        else:
+            # Clean up the cache in the current resolver and the current function
+            _to_be_deleted = []
+            for user, cached_result in CACHE[resolver_id].get(func.func_name).iteritems():
+                if cached_result.get("timestamp") < now + tdelta:
+                    _to_be_deleted.append(user)
+            for user in _to_be_deleted:
+                del CACHE[resolver_id][func.func_name][user]
+            del _to_be_deleted
 
         # get the portion of the cache for this very LDAP resolver
         r_cache = CACHE.get(resolver_id).get(func.func_name)
-        if args[0] in r_cache and \
-                        datetime.datetime.now() < r_cache[args[0]][
-                    "timestamp"] + \
-                        datetime.timedelta(seconds=self.cache_timeout):
+        if args[0] in r_cache and now < r_cache[args[0]]["timestamp"] + tdelta:
             log.debug("Reading {0!r} from cache for {1!r}".format(args[0],
                                                               func.func_name))
             return r_cache[args[0]]["value"]
@@ -209,7 +217,7 @@ def cache(func):
         # now we cache the result
         CACHE[resolver_id][func.func_name][args[0]] = {
             "value": f_result,
-            "timestamp": datetime.datetime.now()}
+            "timestamp": now}
 
         return f_result
 
