b300191324189eac78327d84aff0ca0fd3ad6f54
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2019-02-06 13:42:27 +0100

    Update info for version 2.23.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import stat
 import sys
 
 #VERSION="2.1dev4"
-VERSION="2.23.4dev5"
+VERSION="2.23.4"
 
 # Taken from kennethreitz/requests/setup.py
 package_directory = os.path.realpath(os.path.dirname(__file__))
