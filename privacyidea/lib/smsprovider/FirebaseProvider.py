71bf8d46ba551ee76e1535899b371152ed67b92a
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2019-04-05 11:30:00 +0200

    Clarify the text for the rollout step time

--- privacyidea/lib/smsprovider/FirebaseProvider.py
+++ privacyidea/lib/smsprovider/FirebaseProvider.py
@@ -115,7 +115,7 @@ class FirebaseProvider(ISMSProvider):
                                      ' Usually it is the endpoint /ttype/push of the privacyIDEA server.')},
                       FIREBASE_CONFIG.TTL: {
                           "required": True,
-                          "description": _('How long should the second step of the enrollment be accepted (in minutes).')
+                          "description": _('The second enrollment step must be completed within this time (in minutes).')
                       },
                       FIREBASE_CONFIG.PROJECT_ID: {
                           "required": True,
