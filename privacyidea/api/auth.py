ba8c5452816e53b8c5e6d69a55c4202f3d3389eb
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2019-04-13 00:07:32 +0200

    Add event handler to /auth endpoint
    It is now possible to add an event handler to
    the /auth endpoint for login to the webui.
    But certain information may not be available in this
    API request.
    E.g. the internal administrator is not a user
    that gets identified. Token serials may not be available.
    
    Closes #1567

--- privacyidea/api/auth.py
+++ privacyidea/api/auth.py
@@ -65,6 +65,7 @@ from privacyidea.api.lib.utils import (send_result, get_all_params,
                                        verify_auth_token, getParam)
 from privacyidea.lib.utils import get_client_ip, hexlify_and_unicode
 from privacyidea.lib.config import get_from_config, SYSCONF, update_config_object
+from privacyidea.lib.event import event, EventConfiguration
 from privacyidea.lib import _
 import logging
 
@@ -85,6 +86,7 @@ def before_request():
                          request.host
     g.policy_object = PolicyClass()
     g.audit_object = getAudit(current_app.config)
+    g.event_config = EventConfiguration()
     # access_route contains the ip adresses of all clients, hops and proxies.
     g.client_ip = get_client_ip(request,
                                 get_from_config(SYSCONF.OVERRIDECLIENT))
@@ -99,6 +101,7 @@ def before_request():
 
 @jwtauth.route('', methods=['POST'])
 @postpolicy(get_webui_settings)
+@event("auth", request, g)
 def get_auth_token():
     """
     This call verifies the credentials of the user and issues an
@@ -207,6 +210,8 @@ def get_auth_token():
     loginname, realm = split_user(username)
     realm = realm or get_default_realm()
 
+    user_obj = User()
+
     # Check if the remote user is allowed
     if (request.remote_user == username) and is_remote_user_allowed(request):
         # Authenticated by the Web Server
@@ -274,6 +279,7 @@ def get_auth_token():
                         details=details or {})
     else:
         g.audit_object.log({"success": True})
+        request.User = user_obj
 
     # If the HSM is not ready, we need to create the nonce in another way!
     hsm = init_hsm()
