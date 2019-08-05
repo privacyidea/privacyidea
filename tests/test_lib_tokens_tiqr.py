a8c229c9cf3dd9e9a445a660ac5c7029ef80a679
Bas van der Vlies <bas.vandervlies@surfsara.nl>
2019-08-05 17:49:33 +0200

    TIQR token enhancements
    When we unlock the TIQR token with the App and use the wrong PIN/biometric code. Respond
    back how many retries we have left before the token is locked/blocked
    
    Fix hexadecimal challenge length for TiQR
    
    Provide the service_displayname in the tiqrauth qrcode. Else we get
    unknown identifier in the screen with please unroll first message

--- tests/test_lib_tokens_tiqr.py
+++ tests/test_lib_tokens_tiqr.py
@@ -126,7 +126,7 @@ class OCRASuiteTestCase(MyTestCase):
         # test creation of hex challenge
         os = OCRASuite("OCRA-1:HOTP-SHA1-6:QH10-S128")
         c = os.create_challenge()
-        self.assertEqual(len(c), 20)
+        self.assertEqual(len(c), 10)
         self.assertTrue("G" not in c, c)
 
         # test creation of alphanum challenge
@@ -529,7 +529,7 @@ class TiQRTokenTestCase(MyApiTestCase):
                         "operation": "login"}
         r = TiqrTokenClass.api_endpoint(req, g)
         self.assertEqual(r[0], "plain")
-        self.assertEqual(r[1], "INVALID_RESPONSE")
+        self.assertRegexpMatches(r[1], r"INVALID_RESPONSE:[0-9]+")
 
         # Check that the OTP status is still incorrect
         r = token.check_challenge_response(options={"transaction_id":
@@ -684,7 +684,7 @@ class TiQRTokenTestCase(MyApiTestCase):
                         "operation": "login"}
         r = TiqrTokenClass.api_endpoint(req, g)
         self.assertEqual(r[0], "plain")
-        self.assertEqual(r[1], "INVALID_RESPONSE")
+        self.assertRegexpMatches(r[1], r"INVALID_RESPONSE:[0-9]+")
 
         # Check that the OTP status is still incorrect
         r = token.check_challenge_response(options={"transaction_id":
