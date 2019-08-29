ac250442f73e5c20a480bcce1cf1d0a055424227
Friedrich Weber <friedrich.weber@netknights.it>
2019-08-29 12:45:49 +0200

    Improve docstrings

--- privacyidea/lib/policymatch.py
+++ privacyidea/lib/policymatch.py
@@ -36,6 +36,17 @@ class Match(object):
 
     A ``Match`` object encapsulates a policy matching operation, i.e. a call to ``PolicyClass.match_policies``.
     In order to retrieve the matching policies, one should use one of ``policies()``, ``action_values()`` and ``any()``.
+    By default, these functions write the matched policies to the audit log. This behavior can be explicitly disabled.
+
+    Every classmethod expects a so-called "context object" as its first argument.
+    The context object implements the following attributes:
+     * ``audit_object``: an ``Audit`` object which is used to write the used policies to the audit log.
+                         In case False is passed for ``write_to_audit_log``, the audit object may be None.
+     * ``policy_object``: a ``PolicyClass`` object that is used to retrieve the matching policies.
+     * ``client_ip``: the IP of the current client, as a string
+     * ``logged_in_user``: a dictionary with keys "username", "realm", "role" that describes the
+                           currently logged-in (managing) user
+    In our case, this context object is usually the ``flask.g`` object.
     """
     def __init__(self, g, **kwargs):
         self._g = g
@@ -43,9 +54,10 @@ class Match(object):
 
     def policies(self, write_to_audit_log=True):
         """
-        Return a list of policies, sorted by priority (prioritized policies appear first).
+        Return a list of policies. The list is sorted by priority, which means that prioritized policies appear first.
         :param write_to_audit_log: If True, write the list of matching policies to the audit log
         :return: a list of policy dictionaries
+        :rtype: list
         """
         if write_to_audit_log:
             audit_data = self._g.audit_object.audit_data
@@ -64,14 +76,15 @@ class Match(object):
 
     def action_values(self, unique, allow_white_space_in_action=False, write_to_audit_log=True):
         """
-        Return a dictionary of action values of the matching policies.
+        Return a dictionary of action values extracted from the matching policies.
+        The dictionary maps each action value to a list of policies which define this action value.
         :param unique: If True, return only the prioritized action value.
                        See ``PolicyClass.get_action_values`` for details.
         :param allow_white_space_in_action: If True, allow whitespace in action values.
                        See ``PolicyClass.get_action_values`` for details.
         :param write_to_audit_log: If True, augment the audit log with the names of all
                        policies whose action values are returned
-        :return:
+        :rtype: dict
         """
         policies = self.policies(write_to_audit_log=False)
         action_values = self._g.policy_object.extract_action_values(policies,
@@ -88,10 +101,11 @@ class Match(object):
     @classmethod
     def action_only(cls, g, scope, action):
         """
-        Match policies solely based on a scope and an action.
+        Match active policies solely based on a scope and an action, which may also be None.
+        The client IP is matched implicitly.
         :param g: context object
         :param scope: the policy scope. SCOPE.ADMIN cannot be passed, ``admin`` must be used instead.
-        :param action: the policy action
+        :param action: the policy action, or None
         :rtype: ``Match``
         """
         if scope == SCOPE.ADMIN:
@@ -104,11 +118,12 @@ class Match(object):
     @classmethod
     def realm(cls, g, scope, action, realm):
         """
-        Match policies with a scope, an action and a realm (which must not be None).
+        Match active policies with a scope, an action and a user realm.
+        The client IP is matched implicitly.
         :param g: context object
         :param scope: the policy scope. SCOPE.ADMIN cannot be passed, ``admin`` must be used instead.
         :param action: the policy action
-        :param realm: the realm to match, must not be None
+        :param realm: the realm to match
         :rtype: ``Match``
         """
         if scope == SCOPE.ADMIN:
@@ -121,7 +136,8 @@ class Match(object):
     @classmethod
     def user(cls, g, scope, action, user):
         """
-        Match policies with a scope, an action and a user object (which may be None).
+        Match active policies with a scope, an action and a user object (which may be None).
+        The client IP is matched implicitly.
         :param g: context object
         :param scope: the policy scope. SCOPE.ADMIN cannot be passed, ``admin`` must be used instead.
         :param action: the policy action
@@ -146,10 +162,11 @@ class Match(object):
         Match admin policies with an action and, optionally, a realm.
         Assumes that the currently logged-in user is an admin, and throws an error otherwise.
         Policies will be matched against the admin's username and adminrealm,
-        and optionally also the provided realm.
-        :param g: ``flask.g`` object
+        and optionally also the provided user realm.
+        The client IP is matched implicitly.
+        :param g: context object
         :param action: the policy action
-        :param realm: the realm against which policies should be matched. Can be None.
+        :param realm: the user realm against which policies should be matched. Can be None.
         :type realm: str or None
         :rtype: ``Match``
         """
@@ -168,11 +185,12 @@ class Match(object):
         """
         Depending on the role of the currently logged-in user, match either scope=ADMIN or scope=USER policies.
         If the currently logged-in user is an admin, match policies against the username, adminrealm
-        and the given realm. If the currently logged-in user is a user, match policies against the username
-        and the given realm.
-        :param g: ``flask.g`` object
+        and the given user realm.
+        If the currently logged-in user is a user, match policies against the username and the given realm.
+        The client IP is matched implicitly.
+        :param g: context object
         :param action: the policy action
-        :param realm: the given realm, must not be None
+        :param realm: the given realm
         :rtype: ``Match``
         """
         from privacyidea.lib.auth import ROLE
@@ -184,9 +202,7 @@ class Match(object):
             scope = SCOPE.USER
             username = g.logged_in_user["username"]
             adminrealm = None
-            # TODO: resolver?
         else:
-            # TODO: API key?
             raise MatchingError("Unknown role")
         return cls(g, name=None, scope=scope, realm=realm, active=True,
                    resolver=None, user=username, user_object=None,
