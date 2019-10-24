338dcb15ad8de0a38d73946fa1d945c0ff9454e9
Jean-Pierre <jean-pierre.hoehmann@netknights.it>
2019-10-24 18:10:47 +0200

    Fix a translation issue

--- privacyidea/static/components/token/controllers/tokenLostController.js
+++ privacyidea/static/components/token/controllers/tokenLostController.js
@@ -9,7 +9,7 @@ myApp.controller("tokenLostController", function ($scope,
             $scope.confirm_action_levels["difficult"],
             gettextCatalog.getString("Mark Token as Lost"),
             gettextCatalog.getString("Do you really want to mark this token as lost? " +
-                "This will disable the token and generate a temporary password based token as a replacement!",
+                "This will disable the token and generate a temporary password based token as a replacement!"),
             gettextCatalog.getString("Yes, this token is really lost!"),
             $scope.setTokenLost);
     };
