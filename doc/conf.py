4aa061dd9ec3e1363aeb17d981c2fc4b2a159482
NuvandaPV <jp@hhmn.de>
2020-01-10 16:59:21 +0100

    Load plantUML.

--- doc/conf.py
+++ doc/conf.py
@@ -58,7 +58,7 @@ sys.path.insert(0, os.path.abspath('../privacyidea'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.imgmath', 'sphinx.ext.viewcode', 
-              'sphinxcontrib.autohttp.flask']
+              'sphinxcontrib.autohttp.flask', 'sphinxcontrib.planuml']
 http_index_ignore_prefixes = ['/token']
 
 # Add any paths that contain templates here, relative to this directory.
