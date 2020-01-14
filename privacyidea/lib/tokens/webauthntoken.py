3fde89fc04974196bede55fc7a6c9a5ab134b12c
Jean-Pierre <jean-pierre.hoehmann@netknights.it>
2020-01-14 17:21:13 +0100

    Start fleshing out the API.

--- privacyidea/lib/tokens/webauthntoken.py
+++ privacyidea/lib/tokens/webauthntoken.py
@@ -26,6 +26,8 @@ from privacyidea.models import Challenge
 from privacyidea.lib.tokens.u2ftoken import IMAGES
 from privacyidea.lib.log import log_with
 import logging
+from privacyidea.lib import _
+from privacyidea.lib.policy import SCOPE, GROUP, ACTION
 
 __doc__ = """
 WebAuthn  is the Web Authentication API specified by the FIDO Alliance.
@@ -173,7 +175,11 @@ for the token (without requiring any special permissions).
                 "img": <imageUrl>,
                 "webauthnSignRequest": {
                     "challenge": <nonce>,
-                    "id": <credentialId>,
+                    "allowCredentials": [{
+                        "id": <credentialId>,
+                        "type": <keyType>,
+                        "transports": <allowedTransports>
+                    }],
                     "timeout": <timeout>
                 }
             },
@@ -225,7 +231,11 @@ challenge will be triggered for every challenge response token the user has.
                 "img": <imageUrl>,
                 "webauthnSignRequest": {
                     "challenge": <nonce>,
-                    "id": <credentialId>,
+                    "allowCredentials": [{
+                        "id": <credentialId>,
+                        "type": <keyType>,
+                        "transports": <allowedTransports>
+                    }],
                     "timeout": <timeout>
                 }
             },
@@ -237,7 +247,11 @@ challenge will be triggered for every challenge response token the user has.
                     "img": <imageUrl>,
                     "webauthnSignRequest": {
                         "challenge": <nonce>,
-                        "id": <credentialId>,
+                        "allowCredentials": [{
+                            "id": <credentialId>,
+                            "type": <keyType>,
+                            "transports": <allowedTransports>
+                        }],
                         "timeout": <timeout>
                     }
                 },
@@ -266,18 +280,15 @@ Send the Response
 
 The application now needs to call the javascipt function
 *navigator.credentials.get* with *publicKeyCredentialRequstOptions* built using
-the *nonce*, *credentialId* and *timeout* from the server.
+the *nonce*, *credentialId*, *keyType*, *allowedTransports* and *timeout* from
+the server.
 
     const publicKeyCredentialRequestOptions = {
         challenge: Uing8Array.from(<nonce>, c => c.charCodeAt(0)),
         allowCredentials: [{
             id: Uint8Array.from(<credentialId>, c=> c.charcodeAt(0)),
-            type: 'public-key',
-            transports: [
-                'usb',
-                'ble',
-                'nfc'
-            ]
+            type: <keyType>,
+            transports: <allowedTransports>
         }],
         timeout: <timeout>
     }
@@ -320,7 +331,126 @@ log = logging.getLogger(__name__)
 optional = True
 required = False
 
+class WEBAUTHNACTION(object):
+    ALLOWED_TRANSPORTS = 'webauthn_allowed_transports'
+    TIMEOUT_AUTH = 'webauthn_timeout_auth'
+    TIMEOUT_ENROLL = 'webauthn_timeout_enroll'
+    RELYING_PARTY_NAME = 'webauthn_relying_party_name'
+    RELYING_PARTY_ID = 'webauthn_relying_party_id'
+    PUBLIC_KEY_CREDENTIAL_ALGORITHMS = 'webauthn_public_key_credential_algorithms'
+    AUTHENTICATOR_ATTACHMENT = 'webauthn_authenticator_attachment'
+
 class WebAuthnTokenClass(TokenClass):
     """
     The WebAuthn Token implementation.
     """
+
+    @staticmethod
+    def get_class_type():
+        """
+        Returns the internal token type identifier
+        :return: webauthn
+        :rtype: basestring
+        """
+        return "webauthn"
+
+    @staticmethod
+    def get_class_prefix():
+        """
+        Return the prefix, that is used as a prefix for the serial numbers.
+        :return: WAN
+        :rtype: basestring
+        """
+        return "WAN"
+
+    @staticmethod
+    @log_with(log)
+    def get_class_info(key=None, ret='all'):
+        """
+        returns a subtree of the token definition
+
+        :param key: subsection identifier
+        :type key: string
+        :param ret: default return value, if nothing is found
+        :type ret: user defined
+        :return: subsection if key exists or user defined
+        :rtype: dict or scalar
+        """
+        res = {
+            'type': 'webauthn',
+            'title': 'WebAuthn Token',
+            'description': 'WebAuthn: Enroll a Web Authentication token.',
+            'init': {},
+            'config': {},
+            'user': ['enroll'],
+            # This tokentype is enrollable in the UI for...
+            'ui_enroll': ["admin", "user"],
+            'policy': {
+                SCOPE.AUTH: {
+                    WEBAUTHNACTION.ALLOWED_TRANSPORTS: {
+                        'type': 'str',
+                        'desc': _("Only the specified protocols may be used to speak to WebAuthn tokens.")
+                    },
+                    WEBAUTHNACTION.TIMEOUT_AUTH: {
+                        'type': 'int',
+                        'desc': _("The amount of time in seconds the user has to confirm an authorization request on "
+                                  "his WebAuthn token.")
+                    },
+                    ACTION.CHALLENGETEXT: {
+                        'type': 'str',
+                        'desc': _('Use an alternate challenge text for telling the '
+                                  'user to confirm with his WebAuthn device')
+                    }
+                },
+                SCOPE.ENROLL: {
+                    WEBAUTHNACTION.RELYING_PARTY_NAME: {
+                        'type': 'str',
+                        'desc': _("A human readable name for the organization rolling out WebAuthn tokens."),
+                    },
+                    WEBAUTHNACTION.RELYING_PARTY_ID: {
+                        'type': 'str',
+                        'desc': _("A domain name that is a subset of the respective FQDNs for all the webservices the "
+                                  "users should be able to sign in to using WebAuthn tokens.")
+                    },
+                    WEBAUTHNACTION.TIMEOUT_ENROLL: {
+                        'type': 'int',
+                        'desc': _("The amount of time in seconds the user has to confirm enrollment on his "
+                                  "WebAuthn token.")
+                    },
+                    WEBAUTHNACTION.PUBLIC_KEY_CREDENTIAL_ALGORITHMS: {
+                        'type': 'str',
+                        'desc': _("Which algorithms are acceptable for public key creation for WebAuthn. The "
+                                  "algorithms are given by their number in the COSE registry."),
+                        'group': GROUP.TOKEN
+                    },
+                    WEBAUTHNACTION.AUTHENTICATOR_ATTACHMENT: {
+                        'type': 'str',
+                        'desc': _("Whether to limit roll out of WebAuthn tokens to either only platform attachments, "
+                                  "or only cross-platform attachments."),
+                        'group': GROUP.TOKEN,
+                        'value': [
+                            "platform",
+                            "cross-platform",
+                            "either"
+                        ]
+                    },
+                    ACTION.MAXTOKENUSER: {
+                        'type': 'int',
+                        'desc': _("The user may only have this number of WebAuthn tokens assigned."),
+                        'group': GROUP.TOKEN
+                    },
+                    ACTION.MAXACTIVETOKENUSER: {
+                        'type': 'int',
+                        'desc': _('The user may only have this number of active WebAuthn tokens assigned.'),
+                        'group': GROUP.TOKEN
+                    }
+                }
+            }
+        }
+
+        if key:
+            ret = res.get(key, {})
+        else:
+            if ret == 'all':
+                ret = res
+        return ret
