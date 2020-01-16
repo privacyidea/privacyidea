179e5b5ccf9197a34ab0cd43da081129c4b4caf4
Jean-Pierre <jean-pierre.hoehmann@netknights.it>
2020-01-16 10:19:30 +0100

    Add Duo Security license.

--- privacyidea/lib/tokens/webauthn.py
+++ privacyidea/lib/tokens/webauthn.py
@@ -5,8 +5,6 @@
 # License:  AGPLv3
 # Contact:  https://www.privacyidea.org
 #
-# Copyright (C) 2020 NetKnights GmbH
-#
 # This code is free software; you can redistribute it and/or
 # modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
 # License as published by the Free Software Foundation; either
@@ -20,17 +18,44 @@
 # You should have received a copy of the GNU Affero General Public
 # License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+# This code was originally adapted from the Duo Security implementation of
+# WebAuthn for Python.
+#
+# Copyright (c) 2017 Duo Security, Inc. All rights reserved.
+# License:  BSD-3-Clause
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 
 import logging
 
 __doc__ = """
 Business logic for WebAuthn protocol.
 
-This file contains a partial implementation of the server part of the WebAuthn
-protocol. It currently uses a library written by Duo Security to do most of the
-heavy lifting. The functions in this file that make use of this library will be
-gradually rewritten to implement the necessary functionality themselves,
-allowing us to remove the external dependency.
+This file implements the server part of the WebAuthn protocol.
 
 This file is tested in tests/test_lib_tokens_webauthn.py
 """
