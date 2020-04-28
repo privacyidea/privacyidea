226f6a87e65684fca295485f9d0f015b4790507c
Paul Lettich <paul.lettich@netknights.it>
2020-04-28 13:45:06 +0200

    Update supported Python version to 3.8
    Since the default python 3 version in ubuntu 20.04 is 3.8 we should
    include this version to the testing matrix.

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(
     author_email='cornelius@privacyidea.org',
     url='http://www.privacyidea.org',
     keywords='OTP, two factor authentication, management, security',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.9.*',
     packages=find_packages(),
     scripts=["pi-manage"] + get_scripts("tools"),
     extras_require={
@@ -144,7 +144,8 @@ setup(
                  'Programming Language :: Python :: 3',
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7'
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8'
                  ],
     zip_safe=False,
     long_description=get_file_contents('README.rst')
