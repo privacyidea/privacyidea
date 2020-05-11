140217ba91709fc637edb9f71d88621f4b03dada
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2020-05-11 16:33:49 +0200

    Add whitespaces for pep8

--- tests/test_api_lib_policy.py
+++ tests/test_api_lib_policy.py
@@ -744,8 +744,8 @@ class PrePolicyDecoratorTestCase(MyApiTestCase):
         delete_policy("pincontent")
 
     def test_07c_generate_pin_from_policy(self):
-        content_policies_valid = ['+cn','-s','cns','+ns','[1234567890]','[[]€³@/(]']
-        content_policies_invalid = ['+c-ns','cn-s', '+ns-[1234567890]','-[1234567890]']
+        content_policies_valid = ['+cn', '-s', 'cns', '+ns', '[1234567890]', '[[]€³@/(]']
+        content_policies_invalid = ['+c-ns', 'cn-s', '+ns-[1234567890]', '-[1234567890]']
         pin_size = 3
         for content_policy in content_policies_valid:
             pin = _generate_pin_from_policy(content_policy, size=pin_size)
