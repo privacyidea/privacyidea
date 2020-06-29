033853240e2b9b04909de4985a0e5491d75a5cac
Luis Gallo <gallolp@gmail.com>
2020-06-29 14:21:06 -0300

    Fix sqlresolver connstring empty values "" vs None Fixes #2271

--- privacyidea/lib/resolvers/SQLIdResolver.py
+++ privacyidea/lib/resolvers/SQLIdResolver.py
@@ -619,15 +619,15 @@ class IdResolver (UserIdResolver):
             password = u":{0!s}".format(param.get("Password"))
         if param.get("conParams"):
             conParams = u"?{0!s}".format(param.get("conParams"))
-        connect_string = u"{0!s}://{1!s}{2!s}{3!s}{4!s}{5!s}/{6!s}{7!s}".format(param.get("Driver", ""),
-                                                   param.get("User", ""),
+        connect_string = u"{0!s}://{1!s}{2!s}{3!s}{4!s}{5!s}/{6!s}{7!s}".format(param.get("Driver") or "",
+                                                   param.get("User") or "",
                                                    password,
                                                    "@" if (param.get("User")
                                                            or
                                                            password) else "",
-                                                   param.get("Server", ""),
+                                                   param.get("Server") or "",
                                                    port,
-                                                   param.get("Database", ""),
+                                                   param.get("Database") or "",
                                                    conParams)
         # SQLAlchemy does not like a unicode connect string!
 #        if param.get("Driver").lower() == "sqlite":
