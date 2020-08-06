c5aa9b931e92131bac471028ff989dc4c3988ed5
Bruno Cascio <brunocascio@gmail.com>
2020-08-06 18:34:07 -0300

    feat: Support embebded dictionaries for error handling

--- privacyidea/lib/resolvers/HTTPResolver.py
+++ privacyidea/lib/resolvers/HTTPResolver.py
@@ -232,7 +232,7 @@ class HTTPResolver(UserIdResolver):
 
         if hasSpecialErrorHandler:
             # verify if error response mapping is a subset of the json http response
-            if errorResponse.items() <= jsonHTTPResponse.items():
+            if all([x in jsonHTTPResponse.items() for x in errorResponse.items()]):
                 log.error(jsonHTTPResponse)
                 raise Exception('Received an error while searching for user: %s' % userid)
 
