14607475ff22e6394ef8c1a9349ae05a6c62c08f
Paul Lettich <paul.lettich@netknights.it>
2020-08-11 18:08:17 +0200

    Add tests for dynamic token types

---
+++ tests/testdata/fancytoken.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+""" Test module for a fancy token class """
+
+from privacyidea.lib.tokenclass import TokenClass
+
+
+class FancyTokenClass(TokenClass):
+    def __init__(self, db_token):
+        TokenClass.__init__(self, db_token)
+        self.set_type(u"fancy")
+        self.mode = ['authenticate']
+        self.hKeyRequired = False
+
+    @staticmethod
+    def get_class_type():
+        return 'fancy'
+
+    @staticmethod
+    def get_class_prefix():
+        return "PIFA"
