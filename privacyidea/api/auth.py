2fc1137f235f3153d35f48e7a4d5e9363fa60c0e
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2020-08-22 13:29:37 +0200

    Add test for pre-event user in /auth
    The added test uses a pre-event handler
    to enroll an HOTP token for the user.
    Policy say otppin=userstore and loginmode=privacyIDEA.
    
    The user logs in with his userstore password in the first
    step. Then an HOTP token is created.
    The user then logs in with the transaction_id and the OTP
    value directly in the 2nd step.
    
    Closes #2303

--- privacyidea/api/auth.py
+++ privacyidea/api/auth.py
@@ -106,13 +106,13 @@ def before_request():
 
     username = getParam(request.all_data, "username")
     if username:
-        # We only fill request.user, if we really have a username.
+        # We only fill request.User, if we really have a username.
         # On endpoints like /auth/rights, this is not available
         loginname, realm = split_user(username)
         # overwrite the split realm if we have a realm parameter. Default back to default_realm
         realm = getParam(request.all_data, "realm", default=realm) or realm or get_default_realm()
-        # Prefill the requst.user. This is used by some pre-event handlers
-        request.user = User(loginname, realm)
+        # Prefill the request.User. This is used by some pre-event handlers
+        request.User = User(loginname, realm)
 
 
 @jwtauth.route('', methods=['POST'])
