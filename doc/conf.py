6e6476f57dd7383093a232801a8836d3f50e5d59
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2020-09-04 06:55:58 +0200

    Set version to 3.4dev7

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@
 # built documents.
 #
 # The short X.Y version.
-version = '3.3.3'
+version = '3.4dev7'
 # The full version, including alpha/beta/rc tags.
 #release = '2.16dev5'
 release = version
