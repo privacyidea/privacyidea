386beee1a10cb8b1a358b2372fddf5dbc7effabe
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2020-09-07 20:48:47 +0200

    Set version 3.4

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@
 # built documents.
 #
 # The short X.Y version.
-version = '3.4dev8'
+version = '3.4'
 # The full version, including alpha/beta/rc tags.
 #release = '2.16dev5'
 release = version
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import stat
 import sys
 
 #VERSION = "2.1dev4"
-VERSION = "3.4dev8"
+VERSION = "3.4"
 
 # Taken from kennethreitz/requests/setup.py
 package_directory = os.path.realpath(os.path.dirname(__file__))
