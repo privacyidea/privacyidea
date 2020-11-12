5b0e18588e5a5500eb9d1323798e50d51ada58a3
Henning Hollermann <hollermann@physik.rwth-aachen.de>
2020-11-12 08:35:44 +0100

    review2

--- privacyidea/static/components/audit/controllers/auditControllers.js
+++ privacyidea/static/components/audit/controllers/auditControllers.js
@@ -65,7 +65,7 @@ myApp.controller("auditController", function (AuditFactory, $scope, $rootScope,
             $scope.getParams();
             AuditFactory.get($scope.params, function(data) {
                 $scope.auditdata = data.result.value;
-                if ($scope.auditdata.auditdata[0] && (typeof $scope.audit_columns !== 'undefined')) {
+                if ($scope.auditdata.auditdata[0]) {
                     $scope.audit_columns = Object.keys($scope.auditdata.auditdata[0]);
                 } else {
                     $scope.audit_columns = [];
