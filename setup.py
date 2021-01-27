2e4c1fd3ed831ae876d04287f9832ca436e10020
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2021-01-27 08:57:00 +0100

    set version 3.5.2dev2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import stat
 import sys
 
 #VERSION = "2.1dev4"
-VERSION = "3.5.1dev1"
+VERSION = "3.5.1dev2"
 
 # Taken from kennethreitz/requests/setup.py
 package_directory = os.path.realpath(os.path.dirname(__file__))
