005f820135533fd25a149b40dc5e5e1e423ed711
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2021-03-07 19:53:01 +0100

    Make the tokenwizard only visible in case of 2nd
    The token wizard in the menu should only be visible
    if the policy token_wizard_2nd is set.
    
    Fixes #2632

--- privacyidea/api/lib/postpolicy.py
+++ privacyidea/api/lib/postpolicy.py
@@ -497,9 +497,9 @@ def get_webui_settings(request, response):
                                             user=loginname, realm=realm).action_values(unique=True)
         user_page_size_pol = Match.generic(g, scope=SCOPE.WEBUI, action=ACTION.USERPAGESIZE,
                                            user=loginname, realm=realm).action_values(unique=True)
-        token_wizard_2nd = (role == ROLE.USER
-                            and Match.generic(g, scope=SCOPE.WEBUI, action=ACTION.TOKENWIZARD2ND,
-                                          user=loginname, realm=realm).policies())
+        token_wizard_2nd = bool(role == ROLE.USER
+                                and Match.generic(g, scope=SCOPE.WEBUI, action=ACTION.TOKENWIZARD2ND,
+                                                  user=loginname, realm=realm).policies())
         admin_dashboard = (role == ROLE.ADMIN
                            and Match.generic(g, scope=SCOPE.WEBUI, action=ACTION.ADMIN_DASHBOARD,
                                          user=loginname, realm=realm).any())
