c6495f1a0a4eea1bf709a4629a57c04f2e0e82b9
theo <theodor.franke@outlook.de>
2021-07-09 12:37:34 +0200

    small code-style fixes

--- privacyidea/api/token.py
+++ privacyidea/api/token.py
@@ -98,7 +98,8 @@ from privacyidea.api.lib.prepolicy import (prepolicy, check_base_action,
                                            check_admin_tokenlist,
                                            indexedsecret_force_attribute,
                                            check_admin_tokenlist, webauthntoken_enroll, webauthntoken_allowed,
-                                           webauthntoken_request, required_piv_attestation, check_excluded_tokeninfo_fields)
+                                           webauthntoken_request, required_piv_attestation,
+                                           check_excluded_tokeninfo_fields)
 from privacyidea.api.lib.postpolicy import (save_pin_change,
                                             postpolicy)
 from privacyidea.lib.event import event
