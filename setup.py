6707afeb277ecf16ef53ed9cf9557d0a9f19e9a7
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2022-02-19 08:05:37 +0100

    Add Changelog and set versoin 3.7dev3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import stat
 import sys
 
 #VERSION = "2.1dev4"
-VERSION = "3.7dev2"
+VERSION = "3.7dev3"
 
 # Taken from kennethreitz/requests/setup.py
 package_directory = os.path.realpath(os.path.dirname(__file__))
