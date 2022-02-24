cd3ab395e993fbb5827253e8ae95f7ef9e250f17
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2022-02-24 14:13:54 +0100

    Add further test for code cov.

--- tests/test_lib_tokens_push.py
+++ tests/test_lib_tokens_push.py
@@ -274,7 +274,6 @@ class PushTokenTestCase(MyTestCase):
                                      "Use the polling feature of your privacyIDEA Authenticator App "
                                      "to check for a new Login request.", detail.get("message"))
 
-
             # Our ServiceAccountCredentials mock has been called once, because
             # no access token has been fetched before
             mySA.assert_called_once()
@@ -283,6 +282,26 @@ class PushTokenTestCase(MyTestCase):
             # By default, polling is allowed for push tokens so the corresponding
             # challenge should be available in the challenge table, even though
             # the request to firebase failed.
+            chals = get_challenges(serial=tokenobj.token.serial)
+            self.assertEqual(len(chals), 1, chals)
+            chals[0].delete()
+
+            # Do the same with the parameter "exception", so that we receive an Error on HTTP
+            with self.app.test_request_context('/validate/check',
+                                               method='POST',
+                                               data={"user": "cornelius",
+                                                     "realm": self.realm1,
+                                                     "exception": 1,
+                                                     "pass": "pushpin"}):
+                res = self.app.full_dispatch_request()
+                self.assertEqual(400, res.status_code)
+                result = res.json.get("result")
+                self.assertFalse(result.get("status"))
+                error = result.get("error")
+                self.assertEqual(401, error.get("code"))
+                self.assertEqual("ERR401: Failed to submit message to Firebase service.", error.get("message"))
+
+            # Remove the created challenge
             chals = get_challenges(serial=tokenobj.token.serial)
             self.assertEqual(len(chals), 1, chals)
             chals[0].delete()
@@ -330,6 +349,22 @@ class PushTokenTestCase(MyTestCase):
                     mock_log.assert_called_with("Failed to submit message to Firebase service for token {0!s}."
                                                 .format(serial))
             self.assertEqual(len(get_challenges(serial=tokenobj.token.serial)), 0)
+
+            # Do the same with the parameter "exception", so that we receive an Error on HTTP
+            with self.app.test_request_context('/validate/check',
+                                               method='POST',
+                                               data={"user": "cornelius",
+                                                     "realm": self.realm1,
+                                                     "exception": 1,
+                                                     "pass": "pushpin"}):
+                res = self.app.full_dispatch_request()
+                self.assertEqual(400, res.status_code)
+                result = res.json.get("result")
+                self.assertFalse(result.get("status"))
+                error = result.get("error")
+                self.assertEqual(401, error.get("code"))
+                self.assertEqual("ERR401: Failed to submit message to Firebase service.", error.get("message"))
+
             # Check that the challenge is created if the request to firebase
             # succeeded even though polling is disabled
             # add responses, to simulate the successful communication to firebase
@@ -348,6 +383,7 @@ class PushTokenTestCase(MyTestCase):
                 self.assertTrue(jsonresp.get("result").get("status"))
             self.assertEqual(len(get_challenges(serial=tokenobj.token.serial)), 1)
             get_challenges(serial=tokenobj.token.serial)[0].delete()
+
         remove_token(serial=serial)
         delete_smsgateway(self.firebase_config_name)
         delete_policy('push_poll')
