b6933e8f08cff3f81d719afe9aed0da17115d84e
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2022-02-25 16:11:39 +0100

    Fix tests

--- tests/test_api_token.py
+++ tests/test_api_token.py
@@ -17,7 +17,7 @@ from privacyidea.lib.realm import set_realm
 from privacyidea.lib.user import User
 from privacyidea.lib.event import set_event, delete_event, EventConfiguration
 from privacyidea.lib.caconnector import save_caconnector
-from six.moves.urllib.parse import urlencode
+from six.moves.urllib.parse import urlencode, quote
 from privacyidea.lib.tokenclass import DATE_FORMAT
 from privacyidea.lib.tokenclass import ROLLOUTSTATE
 from privacyidea.lib.tokens.hotptoken import VERIFY_ENROLLMENT_MESSAGE
@@ -1575,18 +1575,18 @@ class APITokenTestCase(MyApiTestCase):
         # check password
         with self.app.test_request_context('/validate/check',
                                            data={"user": "cornelius",
-                                                 "pass": registrationcode},
+                                                 "pass": quote(registrationcode)},
                                            method="POST"):
             res = self.app.full_dispatch_request()
             self.assertTrue(res.status_code == 200, res)
             result = res.json.get("result")
-            self.assertTrue(result.get("value"))
+            self.assertTrue(result.get("value"), registrationcode)
 
         # check password again. THe second time it will fail, since the token
         # does not exist anymore.
         with self.app.test_request_context('/validate/check',
                                            data={"user": "cornelius",
-                                                 "pass": registrationcode},
+                                                 "pass": quote(registrationcode)},
                                            method="POST"):
             res = self.app.full_dispatch_request()
             self.assertTrue(res.status_code == 200, res)
