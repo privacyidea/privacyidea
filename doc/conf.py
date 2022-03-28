fa69d84fae59d828bca5ff202a000cf98b368f1c
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2022-03-28 10:20:10 +0200

    Set version 3.7dev5

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ import functools
 # built documents.
 #
 # The short X.Y version.
-version = '3.6.2'
+version = '3.7dev5'
 # The full version, including alpha/beta/rc tags.
 #release = '2.16dev5'
 release = version
