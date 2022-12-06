9a2074604c826512a89b1ebcf9e6fb5ed16f25e7
jona-samuel <jona-samuel.hoehmann@netknights.it>
2022-12-06 13:38:02 +0100

    Fixing Project_ID

--- privacyidea/lib/smsprovider/FirebaseProvider.py
+++ privacyidea/lib/smsprovider/FirebaseProvider.py
@@ -83,6 +83,7 @@ class FIREBASE_CONFIG:
     REGISTRATION_URL = "registration URL"
     TTL = "time to live"
     JSON_CONFIG = "JSON config file"
+    PROJECT_ID = "projectid"
     HTTPS_PROXY = "httpsproxy"
 
 
@@ -149,17 +150,20 @@ class FirebaseProvider(ISMSProvider):
                        }
             }
 
-        #proxies = {}
-        #if self.smsgateway.option_dict.get(FIREBASE_CONFIG.HTTPS_PROXY):
-        #    proxies["https"] = self.smsgateway.option_dict.get(FIREBASE_CONFIG.HTTPS_PROXY)
-        #url = FIREBASE_URL_SEND.format(self.smsgateway.option_dict.get(FIREBASE_CONFIG.PROJECT_ID))
-        #resp = authed_session.post(url, data=json.dumps(fcm_message), headers=headers, proxies=proxies)
+        proxies = {}
+        if self.smsgateway.option_dict.get(FIREBASE_CONFIG.HTTPS_PROXY):
+            proxies["https"] = self.smsgateway.option_dict.get(FIREBASE_CONFIG.HTTPS_PROXY)
+        a = self.smsgateway.option_dict.get(FIREBASE_CONFIG.JSON_CONFIG)
+        with open(a) as f:
+            server_config = json.load(f)
+        url = FIREBASE_URL_SEND.format(server_config["project_id"])
+        resp = authed_session.post(url, data=json.dumps(fcm_message), headers=headers, proxies=proxies)
 
-        #if resp.status_code == 200:
-        #    log.debug("Message sent successfully to Firebase service.")
-        #    res = True
-        #else:
-        #    log.warning(u"Failed to send message to firebase service: {0!s}".format(resp.text))
+        if resp.status_code == 200:
+            log.debug("Message sent successfully to Firebase service.")
+            res = True
+        else:
+            log.warning(u"Failed to send message to firebase service: {0!s}".format(resp.text))
 
         return res
 
@@ -176,24 +180,10 @@ class FirebaseProvider(ISMSProvider):
         if server_config:
             if server_config.get("type") != "service_account":
                 raise ConfigAdminError(description="The JSON file is not a valid firebase credentials file.")
-            #project_id = self.smsgateway.option_dict.get(FIREBASE_CONFIG.PROJECT_ID)
-            #if server_config.get("project_id") != project_id:
-            #    raise ConfigAdminError(description="The project_id you entered does not match the project_id from the JSON file.")
 
         else:
             raise ConfigAdminError(description="Please check your configuration. Can not load JSON file.")
 
-        # We need at least
-        #         FIREBASE_CONFIG.API_KEY_IOS and FIREBASE_CONFIG.APP_ID_IOS
-        # or
-        #         FIREBASE_CONFIG.API_KEY and FIREBASE_CONFIG.APP_ID
-        #android_configured = bool(self.smsgateway.option_dict.get(FIREBASE_CONFIG.APP_ID)) and \
-        #                     bool(self.smsgateway.option_dict.get(FIREBASE_CONFIG.API_KEY))
-        #ios_configured = bool(self.smsgateway.option_dict.get(FIREBASE_CONFIG.APP_ID_IOS)) and \
-        #                     bool(self.smsgateway.option_dict.get(FIREBASE_CONFIG.API_KEY_IOS))
-        #if not android_configured and not ios_configured:
-        #    raise ConfigAdminError(description="You need to at least configure either app_id and api_key or"
-        #                                       " app_id_ios and api_key_ios.")
 
     @classmethod
     def parameters(cls):
