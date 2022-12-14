b2bf26ab889270d76c6daef27e4280a406cd8c17
Your Name <you@example.com>
2022-12-14 21:40:13 +0100

    One Line to avoid None

--- privacyidea/lib/crypto.py
+++ privacyidea/lib/crypto.py
@@ -313,8 +313,7 @@ def encryptPassword(password):
     """
     hsm = get_hsm()
     try:
-        if password is None:
-            password = ""
+        password = password or ""
         ret = hsm.encrypt_password(to_bytes(password))
     except Exception as exx:  # pragma: no cover
         log.warning(exx)
