3be35478de7f3969903aa0c90a20c038b8739a70
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2023-02-23 15:56:22 +0100

    try fix merge

--- tests/test_api_token.py
+++ tests/test_api_token.py
@@ -2697,6 +2697,55 @@ class APITokenTestCase(MyApiTestCase):
 
         delete_policy("verify_toks1")
 
+    def test_43_init_verify_index_token(self):
+        set_policy("verify_toks1", scope=SCOPE.ENROLL, action="{0!s}=indexedsecret".format(ACTION.VERIFY_ENROLLMENT))
+        # Enroll an indexed secret token
+        SECRET = "ABCDEFGHIJHK"
+        with self.app.test_request_context('/token/init',
+                                           method='POST',
+                                           data={"otpkey": SECRET,
+                                                 "type": "indexedsecret"},
+                                           headers={'Authorization': self.at}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 200, res)
+            detail = res.json.get("detail")
+            secret = detail.get("otpkey").get("value").split("/")[2]
+            result = res.json.get("result")
+            self.assertTrue(result.get("status"))
+            self.assertTrue(result.get("value"))
+            self.assertEqual(SECRET, secret)
+            self.assertEqual(detail.get("rollout_state"), ROLLOUTSTATE.VERIFYPENDING)
+            message = detail.get("verify").get("message")
+            self.assertTrue(message.startswith("Please enter the positions"))
+            serial = detail.get("serial")
+            tokenobj_list = get_tokens(serial=serial)
+            # Check the token rollout state
+            self.assertEqual(tokenobj_list[0].token.rollout_state, ROLLOUTSTATE.VERIFYPENDING)
+            s_pos = message.strip("Please enter the positions ").strip(" from your secret.")
+            positions = [int(x) for x in s_pos.split(",")]
+
+        # Now run the second step: verify enrollment and test again
+        otp = ""
+        for x in positions:
+            otp += secret[x-1]
+        with self.app.test_request_context('/token/init',
+                                           method='POST',
+                                           data={"serial": serial,
+                                                 "type": "indexedsecret",
+                                                 "verify": otp},
+                                           headers={'Authorization': self.at}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 200, res)
+            result = res.json.get("result")
+            detail = res.json.get("detail")
+            self.assertTrue(result.get("status"))
+            self.assertTrue(result.get("value"))
+            tokenobj_list = get_tokens(serial=serial)
+            # Check the token rollout state, it is empty now.
+            self.assertEqual(ROLLOUTSTATE.ENROLLED, tokenobj_list[0].token.rollout_state)
+
+        delete_policy("verify_toks1")
+
     def test_44_init_token_with_required_description(self):
         # set require_description policy with value = 'hotp'
         set_policy(name="require_description",
