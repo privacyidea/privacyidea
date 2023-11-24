e9c96f405ad481e250af8e28d9bf475bb81a5ba0
jona-samuel <jona-samuel.hoehmann@netknights.it>
2023-11-24 10:07:35 +0100

    Copyright

--- privacyidea/cli/privacyideatokenjanitor/findtokens.py
+++ privacyidea/cli/privacyideatokenjanitor/findtokens.py
@@ -44,25 +44,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-# SPDX-FileCopyrightText: (C) 2023 Jona-Samuel Höhmann <jona-samuel.hoehmann@netknights.it>
-#
-# SPDX-License-Identifier: AGPL-3.0-or-later
-#
-# Info: https://privacyidea.org
-#
-# This code is free software: you can redistribute it and/or
-# modify it under the terms of the GNU Affero General Public License
-# as published by the Free Software Foundation, either
-# version 3 of the License, or any later version.
-#
-# This code is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public
-# License along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 import click
 from flask.cli import AppGroup, FlaskGroup
 from click import ClickException
--- privacyidea/cli/privacyideatokenjanitor/loadtokens.py
+++ privacyidea/cli/privacyideatokenjanitor/loadtokens.py
@@ -1,3 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# 2023-11-03 Jona-Samuel Höhmann <jona-samuel.hoehmann@netknights.it>
+#            Migrate to click
+# 2020-11-11 Timo Sturm <timo.sturm@netknights.it>
+#            Select how to validate PSKC imports
+# 2018-02-21 Cornelius Kölbel <cornelius.koelbel@netknights.it>
+#            Allow to import PSKC file
+# 2017-11-21 Cornelius Kölbel <corenlius.koelbel@netknights.it>
+#            export to CSV including usernames
+# 2017-10-18 Cornelius Kölbel <cornelius.koelbel@netknights.it>
+#            Add token export (HOTP and TOTP) to PSKC
+# 2017-05-02 Friedrich Weber <friedrich.weber@netknights.it>
+#            Improve token matching
+# 2017-04-25 Cornelius Kölbel <cornelius.koelbel@netknights.it>
+#
+# Copyright (c) 2017, Cornelius Kölbel
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from this
+# software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE
 
 from flask.cli import AppGroup
 import click
