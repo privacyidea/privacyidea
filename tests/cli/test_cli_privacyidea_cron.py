748bf2e4df2295c6c29c0e21552cb02e7732467d
jona-samuel <jona-samuel.hoehmann@netknights.it>
2024-03-11 10:22:53 +0100

    migrate privacyidea-cron to click

---
+++ tests/cli/test_cli_privacyidea_cron.py
@@ -0,0 +1,15 @@
+from .base import CliTestCase
+from privacyidea.cli.privacyidea_cron.main import cli as privacyidea_cron
+
+
+class PITokenJanitorLoadTestCase(CliTestCase):
+    def test_01_pitokenjanitor_help(self):
+        runner = self.app.test_cli_runner()
+        result = runner.invoke(privacyidea_cron, ["-h"])
+        self.assertIn("Execute all periodic tasks that are scheduled to run.",
+                      result.output, result)
+        self.assertIn("Show a list of available tasks that could be run.",
+                      result.output, result)
+        self.assertIn("Manually run a periodic task",
+                      result.output, result)
+
