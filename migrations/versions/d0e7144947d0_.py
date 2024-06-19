30970b88473703ae6a99aa62d7a878eceaac5f93
Paul Lettich <paul.lettich@netknights.it>
2024-06-19 12:20:08 +0200

    Update unique constraint in resolverrealm table

--- migrations/versions/d0e7144947d0_.py
+++ migrations/versions/d0e7144947d0_.py
@@ -15,18 +15,46 @@ down_revision = 'e3a64b4ca634'
 
 
 def upgrade():
+    migration_context = op.get_context()
     try:
         op.add_column('resolverrealm', sa.Column('node_uuid', sa.Unicode(length=36), nullable=True))
-        op.drop_constraint('rrix_2', 'resolverrealm', type_='unique')
-        op.create_unique_constraint('rrix_2', 'resolverrealm', ['resolver_id', 'realm_id', 'node_uuid'])
     except (OperationalError, ProgrammingError) as exx:
         if "already exists" in str(exx.orig).lower():
             print("Ok, column 'node_uuid' already exists.")
         else:
-            print(exx)
-    except Exception as exx:
+            raise
+    except Exception as _e:
         print("Could not add column 'node_uuid' to table 'resolverrealm'")
-        print(exx)
+        raise
+
+    try:
+        # unfortunately MariaDB does not support removing a constraint with
+        # foreign keys, so we have to remove the foreign keys before.
+        if migration_context.dialect.name in ['mysql', 'mariadb']:
+            bind = op.get_bind()
+            insp = sa.inspect(bind.engine)
+            fks = insp.get_foreign_keys('resolverrealm')
+            for ref_table in ['resolver', 'realm']:
+                fk_name = next((fk['name'] for fk in fks if fk['referred_table'] == ref_table), None)
+                if fk_name:
+                    op.drop_constraint(fk_name, 'resolverrealm', type_='foreignkey')
+        # drop the unique constraint
+        op.drop_constraint('rrix_2', 'resolverrealm', type_='unique')
+        if migration_context.dialect.name in ['mysql', 'mariadb']:
+            # we have to re-create the foreign key constraints
+            for ref_table in ['resolver', 'realm']:
+                op.create_foreign_key(None, 'resolverrealm', ref_table, [f'{ref_table}_id'], ['id'])
+        # and re-create the unique constraint
+        op.create_unique_constraint('rrix_2', 'resolverrealm', ['resolver_id', 'realm_id', 'node_uuid'])
+    except (OperationalError, ProgrammingError) as exx:
+        if "already exists" in str(exx.orig).lower():
+            print("Ok, constraint already exists.")
+            print(exx)
+        else:
+            raise
+    except Exception as _e:
+        print("Could not change constraint 'rrix_2' on table 'resolverrealm'")
+        raise
 
 
 def downgrade():
