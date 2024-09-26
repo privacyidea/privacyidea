20657459f23e4b1c396c47d472bf7656ad9973e0
jona-samuel <jona-samuel.hoehmann@netknights.it>
2024-09-26 15:54:13 +0200

    pitokenjanitor find

--- tests/cli/test_cli_privacyideatokenjanitor.py
+++ tests/cli/test_cli_privacyideatokenjanitor.py
@@ -18,16 +18,16 @@
 # License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from .base import CliTestCase
-from privacyidea.cli.pitokenjanitor import cli as pi_token_janitor
+from privacyidea.cli.privacyideatokenjanitor import cli as pi_token_janitor
 
 
 class PITokenJanitorLoadTestCase(CliTestCase):
     def test_01_pitokenjanitor_help(self):
         runner = self.app.test_cli_runner()
         result = runner.invoke(pi_token_janitor, ["-h"])
-        #self.assertIn("Loads token data from the PSKC file.",
-        #              result.output, result)
-        #self.assertIn("Update existing tokens in the privacyIDEA system.",
-        #              result.output, result)
+        self.assertIn("Loads token data from the PSKC file.",
+                      result.output, result)
+        self.assertIn("Update existing tokens in the privacyIDEA system.",
+                      result.output, result)
         self.assertIn("Finds all tokens which match the conditions.",
                       result.output, result)
