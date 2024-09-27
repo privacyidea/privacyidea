76a7085dfcfb5c78840477c0985fead461a2258d
Paul Lettich <paul.lettich@netknights.it>
2024-09-27 15:22:32 +0200

    Prepare for release v3.10.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import stat
 import sys
 
 #VERSION = "2.1dev4"
-VERSION = "3.10"
+VERSION = "3.10.1dev1"
 
 # Taken from kennethreitz/requests/setup.py
 package_directory = os.path.realpath(os.path.dirname(__file__))
