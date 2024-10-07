af5de2bc25ad6301679f5c7223c5b25db85f19c9
henrik-falk <henrik.falk@netknights.it>
2024-10-07 10:32:46 +0200

    refactor test_resolversz and cover failing resolvers

--- tests/test_api_healthcheck.py
+++ tests/test_api_healthcheck.py
@@ -48,66 +48,72 @@ class APIHealthcheckTestCase(MyApiTestCase):
 
     @ldap3mock.activate
     def test_resolversz(self):
+        def check_resolvers(res, expected_status_code, expected_status, ldap_expected_status,
+                            sql_expected_status):
+            assert res.status_code == expected_status_code, f"Expected status code {expected_status_code}, got {res.status_code}"
+            result = res.json.get("result")
+            assert result is not None, "Expected JSON result, got None"
+            assert result.get("value").get(
+                "status") == expected_status, f"Expected '{expected_status}' status, got {result.get('value').get('status')}"
+
+            ldap_resolvers = result.get("value").get("ldapresolvers")
+            sql_resolvers = result.get("value").get("sqlresolvers")
+
+            assert ldap_resolvers is not None, "Expected 'ldapresolvers' in result, but got None"
+            assert sql_resolvers is not None, "Expected 'sqlresolvers' in result, but got None"
+
+            assert all(status == ldap_expected_status for status in
+                       ldap_resolvers.values()), f"At least one LDAP resolver does not have '{ldap_expected_status}' status."
+            assert all(status == sql_expected_status for status in
+                       sql_resolvers.values()), f"At least one SQL resolver does not have '{sql_expected_status}' status."
+
         ldap3mock.setLDAPDirectory(LDAPDirectory)
-
-        ldapr = save_resolver({
-            'LDAPURI': 'ldap://localhost',
-            'LDAPBASE': 'o=test',
-            'BINDDN': 'cn=manager,ou=example,o=test',
-            'BINDPW': 'ldaptest',
-            'LOGINNAMEATTRIBUTE': 'cn',
-            'LDAPSEARCHFILTER': '(cn=*)',
-            'MULTIVALUEATTRIBUTES': '["groups"]',
-            'USERINFO': '{ "username": "cn",'
-                        '"phone" : "telephoneNumber", '
-                        '"mobile" : "mobile", '
-                        '"email" : "mail", '
-                        '"surname" : "sn", '
-                        '"groups": "memberOf", '
-                        '"givenname" : "givenName" }',
-            'UIDTYPE': 'DN',
-            "resolver": "test_ldapresolver",
-            "type": "ldapresolver"
-        })
-
-        sqlr = save_resolver({
-            'Driver': 'sqlite',
-            'Server': '/tests/testdata/',
-            'Database': "testuser.sqlite",
-            'Table': 'users',
-            'Encoding': 'utf8',
-            'Editable': True,
-            'Map': '{ "username": "username", \
-                         "userid" : "id", \
-                         "email" : "email", \
-                         "surname" : "name", \
-                         "givenname" : "givenname", \
-                         "password" : "password", \
-                         "phone": "phone", \
-                         "mobile": "mobile"}',
-            'resolver': "test_sqlresolver",
-            'type': "sqlresolver"
-        })
-
-        self.assertGreater(ldapr, 0, "LDAP resolver creation failed.")
-        self.assertGreater(sqlr, 0, "SQL resolver creation failed.")
 
         with self.app.test_request_context('/healthz/resolversz', method='GET'):
             res = self.app.full_dispatch_request()
-            self.assertEqual(res.status_code, 200, f"Expected status code 200, got {res.status_code}")
-            result = res.json.get("result")
-            self.assertIsNotNone(result, "Expected JSON result, but got None")
-            self.assertEqual(result.get("value").get("status"), "OK",
-                             f"Expected 'OK' status, got {result.get('value').get('status')}")
-            ldap_resolvers = result.get("value").get("ldapresolvers")
-            sql_resolvers = result.get("value").get("sqlresolvers")
-            self.assertIsNotNone(ldap_resolvers, "Expected 'ldapresolvers' in result, but got None")
-            self.assertIsNotNone(sql_resolvers, "Expected 'sqlresolvers' in result, but got None")
-            self.assertTrue(
-                all(status == "OK" for status in ldap_resolvers.values()),
-                "At least one LDAP resolver does not have 'OK' status."
-            )
-            self.assertTrue(
-                all(status == "OK" for status in sql_resolvers.values()),
-                "At least one SQL resolvers does not have 'OK' status."
-            )
+            check_resolvers(res, 200, "OK", ldap_expected_status="fail", sql_expected_status="fail")
+
+            ldapr = save_resolver({
+                'LDAPURI': 'ldap://localhost',
+                'LDAPBASE': 'o=test',
+                'BINDDN': 'cn=manager,ou=example,o=test',
+                'BINDPW': 'ldaptest',
+                'LOGINNAMEATTRIBUTE': 'cn',
+                'LDAPSEARCHFILTER': '(cn=*)',
+                'MULTIVALUEATTRIBUTES': '["groups"]',
+                'USERINFO': '{ "username": "cn",'
+                            '"phone" : "telephoneNumber", '
+                            '"mobile" : "mobile", '
+                            '"email" : "mail", '
+                            '"surname" : "sn", '
+                            '"groups": "memberOf", '
+                            '"givenname" : "givenName" }',
+                'UIDTYPE': 'DN',
+                "resolver": "test_ldapresolver",
+                "type": "ldapresolver"
+            })
+
+            sqlr = save_resolver({
+                'Driver': 'sqlite',
+                'Server': '/tests/testdata/',
+                'Database': "testuser.sqlite",
+                'Table': 'users',
+                'Encoding': 'utf8',
+                'Editable': True,
+                'Map': '{ "username": "username", \
+                             "userid" : "id", \
+                             "email" : "email", \
+                             "surname" : "name", \
+                             "givenname" : "givenname", \
+                             "password" : "password", \
+                             "phone": "phone", \
+                             "mobile": "mobile"}',
+                'resolver': "test_sqlresolver",
+                'type': "sqlresolver"
+            })
+
+            self.assertGreater(ldapr, 0, "LDAP resolver creation failed.")
+            self.assertGreater(sqlr, 0, "SQL resolver creation failed.")
+
+            res = self.app.full_dispatch_request()
+            check_resolvers(res, 200, "OK", ldap_expected_status="OK", sql_expected_status="OK")
