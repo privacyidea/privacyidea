10b553e5ecb9f6eeb8f8c860a31942acca23b7f7
henrik-falk <henrik.falk@netknights.it>
2024-10-29 09:19:38 +0100

    add environment for aes key

--- privacyidea/static/src/app/services/local/local.service.ts
+++ privacyidea/static/src/app/services/local/local.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
 import * as CryptoJS from 'crypto-js';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalService {
 
-  key = "SuperSecretAESKey";
+  key = environment.secretAESKey;
 
   constructor() {
   }
