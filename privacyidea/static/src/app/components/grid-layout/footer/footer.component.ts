710615dca601dda20c8fc3ec3b0d5a2914fe3a54
henrik-falk <henrik.falk@netknights.it>
2024-12-02 15:44:56 +0100

    add version service

--- privacyidea/static/src/app/components/grid-layout/footer/footer.component.ts
+++ privacyidea/static/src/app/components/grid-layout/footer/footer.component.ts
@@ -1,17 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
-import {MatFabAnchor, MatFabButton} from '@angular/material/button';
+import {MatButtonModule} from '@angular/material/button';
+import {VersionService} from '../../../services/version/version.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
     MatIconModule,
-    MatFabButton,
-    MatFabAnchor
+    MatButtonModule
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  version!: string;
+
+  constructor(private versioningService: VersionService) {
+  }
+
+  ngOnInit(): void {
+    this.version = this.versioningService.getVersion();
+  }
 }
---
+++ privacyidea/static/src/app/services/version/version.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VersionService {
+  private version: string;
+
+  constructor() {
+    // TODO get version from an environment variable or API
+    this.version = '3.12';
+  }
+
+  getVersion(): string {
+    return this.version;
+  }
+}
