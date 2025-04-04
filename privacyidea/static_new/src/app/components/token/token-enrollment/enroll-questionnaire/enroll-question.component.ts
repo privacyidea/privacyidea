28aef713e64f71f2b107e4ae97fee085bb247435
henrik-falk <henrik.falk@netknights.it>
2025-04-04 15:34:26 +0200

    refactor get questionnaire questions from system api

--- privacyidea/static_new/src/app/components/token/token-enrollment/enroll-questionnaire/enroll-question.component.ts
+++ privacyidea/static_new/src/app/components/token/token-enrollment/enroll-questionnaire/enroll-question.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, WritableSignal } from '@angular/core';
+import { Component, Input, signal, WritableSignal } from '@angular/core';
 import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TokenService } from '../../../../services/token/token.service';
+import { SystemService } from '../../../../services/system/system.service';
 
 @Component({
   selector: 'app-enroll-question',
@@ -18,17 +19,8 @@ import { TokenService } from '../../../../services/token/token.service';
   styleUrl: './enroll-question.component.scss',
 })
 export class EnrollQuestionComponent {
-  configQuestions = [
-    // TODO get this from system endpoint
-    'What is your favorite color?',
-    'What is your favorite animal?',
-    'What is your favorite food?',
-    'What is your favorite movie?',
-    'What is your favorite book?',
-    'What is your favorite song?',
-    'What is your favorite sport?',
-  ];
-  configMinNumberOfAnswers = 3;
+  configQuestions = signal<{ question: string; text: unknown }[]>([]);
+  configMinNumberOfAnswers = signal(0);
 
   text = this.tokenService
     .tokenTypeOptions()
@@ -36,7 +28,34 @@ export class EnrollQuestionComponent {
   @Input() description!: WritableSignal<string>;
   @Input() answers!: WritableSignal<Record<string, string>>;
 
-  constructor(private tokenService: TokenService) {}
+  constructor(
+    private tokenService: TokenService,
+    private systemService: SystemService,
+  ) {
+    this.systemService.getSystemConfig().subscribe((response) => {
+      const config = response?.result?.value;
+      console.log('System config:', config);
+
+      if (
+        config &&
+        Object.keys(config).some((key) => key.startsWith('question.question.'))
+      ) {
+        const questions = Object.entries(config)
+          .filter(([key]) => key.startsWith('question.question.'))
+          .map(([key, value]) => ({
+            question: key.replace('question.question.', ''),
+            text: value,
+          }));
+        this.configQuestions.set(questions);
+
+        if (config['question.num_answers']) {
+          this.configMinNumberOfAnswers.set(
+            parseInt(config['question.num_answers'], 10),
+          );
+        }
+      }
+    });
+  }
 
   onAnswerChange(newValue: string, question: string): void {
     this.answers.update((currentAnswers) => ({
@@ -51,7 +70,8 @@ export class EnrollQuestionComponent {
       (answer) => answer && answer.trim() !== '',
     ).length;
     return (
-      !currentAnswers[question] && answeredCount < this.configMinNumberOfAnswers
+      !currentAnswers[question] &&
+      answeredCount < this.configMinNumberOfAnswers()
     );
   }
 }
