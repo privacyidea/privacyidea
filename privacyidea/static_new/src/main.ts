5e23e94f83466fc56c530257b401a9d1316e5dd7
frankmer <frank.merkel@netknights.it>
2025-08-19 14:00:27 +0200

    feat(i18n): add internationalization support to various components and templates

--- privacyidea/static_new/src/main.ts
+++ privacyidea/static_new/src/main.ts
@@ -1,3 +1,5 @@
+/// <reference types="@angular/localize" />
+
 import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
