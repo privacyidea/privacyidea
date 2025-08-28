5189041d083c3c349d332f9dcf828161e09199e4
henrik-falk <henrik.falk@netknights.it>
2025-08-28 14:19:30 +0200

    reformat

--- privacyidea/static_new/src/app/components/audit/audit.self-service.component.ts
+++ privacyidea/static_new/src/app/components/audit/audit.self-service.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
+import { Component } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
 import {
   MatCell,
   MatCellDef,
@@ -12,24 +12,22 @@ import {
   MatRow,
   MatRowDef,
   MatTable
-} from '@angular/material/table';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
-import { MatInput } from '@angular/material/input';
-import { NgClass } from '@angular/common';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatDrawer, MatDrawerContainer, MatDrawerContent } from '@angular/material/sidenav';
-import { MatIcon } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import {
-  NavigationSelfServiceComponent
-} from '../token/navigation-self-service/navigation-self-service.component';
-import { RouterLink } from '@angular/router';
-import { ClearableInputComponent } from '../shared/clearable-input/clearable-input.component';
-import { CopyButtonComponent } from '../shared/copy-button/copy-button.component';
-import { ScrollToTopDirective } from '../shared/directives/app-scroll-to-top.directive';
-import { KeywordFilterComponent } from '../shared/keyword-filter/keyword-filter.component';
-import { AuditComponent } from './audit.component';
+} from "@angular/material/table";
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
+import { MatInput } from "@angular/material/input";
+import { NgClass } from "@angular/common";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatDrawer, MatDrawerContainer, MatDrawerContent } from "@angular/material/sidenav";
+import { MatIcon } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { NavigationSelfServiceComponent } from "../token/navigation-self-service/navigation-self-service.component";
+import { RouterLink } from "@angular/router";
+import { ClearableInputComponent } from "../shared/clearable-input/clearable-input.component";
+import { CopyButtonComponent } from "../shared/copy-button/copy-button.component";
+import { ScrollToTopDirective } from "../shared/directives/app-scroll-to-top.directive";
+import { KeywordFilterComponent } from "../shared/keyword-filter/keyword-filter.component";
+import { AuditComponent } from "./audit.component";
 
 @Component({
   selector: "app-audit-self-service",
