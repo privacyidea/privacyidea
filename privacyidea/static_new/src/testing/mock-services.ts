2fc963525cd4b566393f81fd054d1b9c7dfe66c8
henrik-falk <henrik.falk@netknights.it>
2025-09-17 17:58:16 +0200

    more coverage

--- privacyidea/static_new/src/testing/mock-services.ts
+++ privacyidea/static_new/src/testing/mock-services.ts
@@ -1137,3 +1137,25 @@ export class MockLoadingService {
   addLoading = jest.fn();
   removeLoading = jest.fn();
 }
+
+type TestApplicationsShape = {
+  ssh: {
+    options: {
+      sshkey: {
+        service_id: { value: string[] };
+      };
+    };
+  };
+};
+
+export class MockApplicationService {
+  applications: WritableSignal<TestApplicationsShape> = signal({
+    ssh: {
+      options: {
+        sshkey: {
+          service_id: { value: ["svc-1", "svc-2"] }
+        }
+      }
+    }
+  });
+}
