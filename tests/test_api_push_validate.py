7eb1669159cb2ff8630cb1a3a6bcf92f67b0b380
Paul Lettich <paul.lettich@netknights.it>
2025-09-25 03:37:26 +0200

    Disable require_presence when push_wait is active
    Since there is no possibility to pass the valid presence question to the
    user, the 'require_presence' policy will be disabled in this case and a
    warning will be written to the log.
    
    Also fixed the timestamp check since it was not working as advertised.
    
    Closes #4702

--- tests/test_api_push_validate.py
+++ tests/test_api_push_validate.py
@@ -1,20 +1,31 @@
-from .base import MyApiTestCase
-from privacyidea.lib.user import User
-from privacyidea.lib.token import get_tokens, init_token, remove_token, get_one_token
-from privacyidea.lib.policy import SCOPE, set_policy, delete_policy
-from privacyidea.lib.tokens.pushtoken import PUSH_ACTION, strip_key
-from privacyidea.lib.smsprovider.SMSProvider import set_smsgateway
-from privacyidea.lib.smsprovider.FirebaseProvider import FirebaseConfig
-from cryptography.hazmat.primitives import serialization
+# SPDX-FileCopyrightText: 2020 NetKnights GmbH <https://netknights.it>
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from base64 import b32encode
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
 from cryptography.hazmat.backends import default_backend
-from privacyidea.lib.utils import to_bytes, to_unicode
-from cryptography.hazmat.primitives.asymmetric import rsa
+import datetime
+import logging
+
+from testfixtures import LogCapture
+
+from .base import MyApiTestCase
+from privacyidea.lib.user import User
+from privacyidea.lib.token import (get_tokens, init_token, remove_token,
+                                   get_one_token, enable_token)
+from privacyidea.lib.tokenclass import CLIENTMODE, ROLLOUTSTATE
+from privacyidea.lib.policy import SCOPE, set_policy, delete_policy
+from privacyidea.lib.tokens.pushtoken import (PUSH_ACTION, strip_key, POLL_ONLY,
+                                              DEFAULT_CHALLENGE_TEXT)
+from privacyidea.lib.challenge import get_challenges
+from privacyidea.lib.smsprovider.SMSProvider import set_smsgateway
+from privacyidea.lib.smsprovider.FirebaseProvider import FirebaseConfig
+from privacyidea.lib.utils import to_bytes, to_unicode, AUTH_RESPONSE
 from privacyidea.lib.policies.actions import PolicyAction
-from privacyidea.lib.token import enable_token
 from privacyidea.lib.realm import set_realm, set_default_realm
 from privacyidea.lib.resolver import save_resolver
-from privacyidea.lib.tokenclass import CLIENTMODE
 from . import ldap3mock
 
 
@@ -319,13 +330,11 @@ class PushAPITestCase(MyApiTestCase):
             res = self.app.full_dispatch_request()
             self.assertEqual(res.status_code, 200)
             detail = res.json.get("detail")
-            serial = detail.get("serial")
             self.assertEqual(detail.get("rollout_state"), "clientwait")
             self.assertTrue("pushurl" in detail)
             # check that the new URL contains the serial number
             self.assertTrue("&serial=PIPU" in detail.get("pushurl").get("value"))
             self.assertFalse("otpkey" in detail)
-            enrollment_credential = detail.get("enrollment_credential")
 
         # We skip the 2nd step of the enrollment!
         # But we activate the token on purpose!
@@ -504,7 +513,6 @@ class PushAPITestCase(MyApiTestCase):
             res = self.app.full_dispatch_request()
             self.assertTrue(res.status_code == 200, res)
             result = res.json.get("result")
-            detail = res.json.get("detail")
             self.assertTrue(result.get("status"))
             self.assertTrue(result.get("value"))
             self.assertEqual(result.get("authentication"), "ACCEPT")
@@ -514,3 +522,248 @@ class PushAPITestCase(MyApiTestCase):
         delete_policy("pol_multienroll")
         delete_policy("pol_push2")
         remove_token(serial)
+
+    def test_15_push_with_require_presence(self):
+        self.setUp_user_realms()
+        # Setup PUSH policies
+        set_policy("push_config", scope=SCOPE.ENROLL,
+                   action="{0!s}={1!s},{2!s}={3!s}".format(
+                       PUSH_ACTION.FIREBASE_CONFIG, POLL_ONLY,
+                       PUSH_ACTION.REGISTRATION_URL, REGISTRATION_URL))
+        # Add the require_presence policy
+        set_policy("push_require_presence", scope=SCOPE.AUTH,
+                   action=f"{PUSH_ACTION.REQUIRE_PRESENCE}=1")
+        # Create push token for user
+        # 1st step
+        with self.app.test_request_context('/token/init',
+                                           method='POST',
+                                           data={"type": "push",
+                                                 "pin": "push_pin",
+                                                 "user": "selfservice",
+                                                 "realm": self.realm1,
+                                                 "serial": self.serial_push,
+                                                 "genkey": 1},
+                                           headers={'Authorization': self.at}):
+            res = self.app.full_dispatch_request()
+            detail = res.json.get("detail")
+            enrollment_credential = detail.get("enrollment_credential")
+
+        # 2nd step: as performed by the smartphone
+        with self.app.test_request_context('/ttype/push',
+                                           method='POST',
+                                           data={"enrollment_credential": enrollment_credential,
+                                                 "serial": self.serial_push,
+                                                 "pubkey": self.smartphone_public_key_pem_urlsafe,
+                                                 "fbtoken": "firebaseT"}):
+            res = self.app.full_dispatch_request()
+            self.assertEqual(200, res.status_code, res)
+            self.assertTrue(res.json.get("result").get("status"), res.json)
+            self.assertTrue(res.json.get("result").get("value"), res.json)
+            detail = res.json.get("detail")
+            # Now the smartphone gets a public key from the server
+            self.assertIn("public_key", detail, detail)
+            self.assertEqual(ROLLOUTSTATE.ENROLLED, detail.get("rollout_state"), detail)
+
+        #############################################################
+        # Run authentication with push token and without push_wait
+        with self.app.test_request_context('/validate/check',
+                                           method='POST',
+                                           data={"user": "selfservice",
+                                                 "pass": "push_pin"}):
+            res = self.app.full_dispatch_request()
+            self.assertTrue(res.status_code == 200, res)
+            detail = res.json.get("detail")
+            # Get the challenge data
+            transaction_id = detail.get("transaction_id")
+            challenge_messages = [m.strip() for m in detail.get("message").split(",")]
+            challenge = get_challenges(serial=self.serial_push, transaction_id=transaction_id)[0]
+            # The correct answer is always appended to the available options
+            presence_answer = challenge.get_data().split(',').pop()
+            # Check that we get a presence required message
+            challenge_text = DEFAULT_CHALLENGE_TEXT + f" Please press: {presence_answer}"
+            self.assertTrue(challenge_text in challenge_messages)
+
+        # We do poll only, so we need to poll
+        timestamp = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+        sign_string = "{serial}|{timestamp}".format(serial=self.serial_push,
+                                                    timestamp=timestamp)
+        sig = self.smartphone_private_key.sign(sign_string.encode('utf8'),
+                                               padding.PKCS1v15(),
+                                               hashes.SHA256())
+        # now check that we receive the challenge when polling
+        with self.app.test_request_context('/ttype/push',
+                                           method='GET',
+                                           query_string={"serial": self.serial_push,
+                                                         "timestamp": timestamp,
+                                                         "signature": b32encode(sig)}):
+            res = self.app.full_dispatch_request()
+            self.assertEqual(200, res.status_code, res)
+            value = res.json.get("result").get("value")
+            self.assertEqual("Do you want to confirm the login?", value[0].get("question"))
+            nonce = value[0].get("nonce")
+        # Answer the challenge without presence option
+        sign_string = f"{nonce}|{self.serial_push}"
+        sig = self.smartphone_private_key.sign(sign_string.encode('utf8'),
+                                               padding.PKCS1v15(),
+                                               hashes.SHA256())
+        with LogCapture(level=logging.WARNING) as lc:
+            with self.app.test_request_context('/ttype/push',
+                                               method='POST',
+                                               query_string={"serial": self.serial_push,
+                                                             "timestamp": timestamp,
+                                                             "signature": b32encode(sig)}):
+                res = self.app.full_dispatch_request()
+                self.assertEqual(200, res.status_code, res)
+                self.assertTrue(res.json.get("result").get("status"), res.json)
+                # This fails since no presence_answer was given
+                self.assertFalse(res.json.get("result").get("value"), res.json)
+                lc.check_present(("privacyidea.lib.tokens.pushtoken", "WARNING",
+                                  "'push_require_presence' Policy is set but the presence "
+                                  "answer is not present in the smartphone request!"))
+        # Finalize authentication fails since the challenge has not been answered
+        with self.app.test_request_context('/validate/check',
+                                           method='POST',
+                                           data={"user": "selfservice", "pass": "",
+                                                 "transaction_id": transaction_id},
+                                           headers={"user_agent": "privacyidea-cp/2.0"}):
+            res = self.app.full_dispatch_request()
+            self.assertEqual(200, res.status_code, res)
+            self.assertTrue(res.json.get("result").get("status"), res.json)
+            self.assertFalse(res.json.get("result").get("value"), res.json)
+            self.assertEqual("Response did not match the challenge.",
+                             res.json.get("detail").get("message"), res.json)
+            self.assertEqual(AUTH_RESPONSE.REJECT,
+                             res.json.get("result").get("authentication"), res.json)
+
+        # Answer the Challenge with the wrong presence_answer
+        # Shift the presence answer character one to the right
+        wrong_answer = chr(((ord(presence_answer) + 1 - 65) % 26) + 65)
+        sign_string = f"{nonce}|{self.serial_push}|{wrong_answer}"
+        sig = self.smartphone_private_key.sign(sign_string.encode('utf8'),
+                                               padding.PKCS1v15(),
+                                               hashes.SHA256())
+        with self.app.test_request_context('/ttype/push',
+                                           method='POST',
+                                           query_string={"serial": self.serial_push,
+                                                         "timestamp": timestamp,
+                                                         "presence_answer": wrong_answer,
+                                                         "signature": b32encode(sig)}):
+            res = self.app.full_dispatch_request()
+            self.assertEqual(200, res.status_code, res)
+            self.assertTrue(res.json.get("result").get("status"), res.json)
+            # This fails since the wrong presence_answer was given
+            self.assertFalse(res.json.get("result").get("value"), res.json)
+        # Finalize authentication still fails since the wrong answer is given
+        with self.app.test_request_context('/validate/check',
+                                           method='POST',
+                                           data={"user": "selfservice", "pass": "",
+                                                 "transaction_id": transaction_id},
+                                           headers={"user_agent": "privacyidea-cp/2.0"}):
+            res = self.app.full_dispatch_request()
+            self.assertEqual(200, res.status_code, res)
+            self.assertTrue(res.json.get("result").get("status"), res.json)
+            self.assertFalse(res.json.get("result").get("value"), res.json)
+            self.assertEqual("Response did not match the challenge.",
+                             res.json.get("detail").get("message"), res.json)
+            self.assertEqual(AUTH_RESPONSE.REJECT,
+                             res.json.get("result").get("authentication"), res.json)
+
+        # Answer the Challenge with the correct answer
+        sign_string = f"{nonce}|{self.serial_push}|{presence_answer}"
+        sig = self.smartphone_private_key.sign(sign_string.encode('utf8'),
+                                               padding.PKCS1v15(),
+                                               hashes.SHA256())
+        with self.app.test_request_context('/ttype/push',
+                                           method='POST',
+                                           query_string={"serial": self.serial_push,
+                                                         "timestamp": timestamp,
+                                                         "presence_answer": presence_answer,
+                                                         "signature": b32encode(sig)}):
+            res = self.app.full_dispatch_request()
+            self.assertEqual(200, res.status_code, res)
+            self.assertTrue(res.json.get("result").get("status"), res.json)
+            self.assertTrue(res.json.get("result").get("value"), res.json)
+        # Finalize authentication
+        with self.app.test_request_context('/validate/check',
+                                           method='POST',
+                                           data={"user": "selfservice", "pass": "",
+                                                 "transaction_id": transaction_id},
+                                           headers={"user_agent": "privacyidea-cp/2.0"}):
+            res = self.app.full_dispatch_request()
+            self.assertEqual(200, res.status_code, res)
+            self.assertTrue(res.json.get("result").get("status"), res.json)
+            self.assertTrue(res.json.get("result").get("value"), res.json)
+            self.assertEqual(AUTH_RESPONSE.ACCEPT,
+                             res.json.get("result").get("authentication"), res.json)
+
+        remove_token(self.serial_push)
+        delete_policy("push_config")
+        delete_policy("push_require_presence")
+
+    def test_16_push_require_presence_with_push_wait(self):
+        self.setUp_user_realms()
+        # Setup PUSH policies
+        set_policy("pol_push_config", scope=SCOPE.ENROLL,
+                   action="{0!s}={1!s},{2!s}={3!s}".format(
+                       PUSH_ACTION.FIREBASE_CONFIG, POLL_ONLY,
+                       PUSH_ACTION.REGISTRATION_URL, REGISTRATION_URL))
+        # Add the require_presence policy
+        set_policy("pol_push_require_presence", scope=SCOPE.AUTH,
+                   action=f"{PUSH_ACTION.REQUIRE_PRESENCE}=1")
+        # Add the push_wait policy (set timeout to 1 second to avoid blocking the tests)
+        set_policy("pol_push_wait", scope=SCOPE.AUTH,
+                   action=f"{PUSH_ACTION.WAIT}=1")
+        # Create push token for user
+        # 1st step
+        with self.app.test_request_context('/token/init',
+                                           method='POST',
+                                           data={"type": "push",
+                                                 "pin": "push_pin",
+                                                 "user": "selfservice",
+                                                 "realm": self.realm1,
+                                                 "serial": self.serial_push,
+                                                 "genkey": 1},
+                                           headers={'Authorization': self.at}):
+            res = self.app.full_dispatch_request()
+            detail = res.json.get("detail")
+            enrollment_credential = detail.get("enrollment_credential")
+
+        # 2nd step: as performed by the smartphone
+        with self.app.test_request_context('/ttype/push',
+                                           method='POST',
+                                           data={"enrollment_credential": enrollment_credential,
+                                                 "serial": self.serial_push,
+                                                 "pubkey": self.smartphone_public_key_pem_urlsafe,
+                                                 "fbtoken": "firebaseT"}):
+            res = self.app.full_dispatch_request()
+            self.assertEqual(200, res.status_code, res)
+            self.assertTrue(res.json.get("result").get("status"), res.json)
+            self.assertTrue(res.json.get("result").get("value"), res.json)
+            detail = res.json.get("detail")
+            # Now the smartphone gets a public key from the server
+            self.assertIn("public_key", detail, detail)
+            self.assertEqual(ROLLOUTSTATE.ENROLLED, detail.get("rollout_state"), detail)
+
+        #############################################################
+        # Run authentication with push token and without push_wait
+        with LogCapture(level=logging.WARNING) as lc:
+            with self.app.test_request_context('/validate/check',
+                                               method='POST',
+                                               data={"user": "selfservice",
+                                                     "pass": "push_pin"}):
+                res = self.app.full_dispatch_request()
+                self.assertEqual(200, res.status_code, res)
+                detail = res.json.get("detail")
+                self.assertTrue(res.json.get("result").get("status"), res.json)
+                self.assertFalse(res.json.get("result").get("value"), res.json)
+                self.assertEqual(AUTH_RESPONSE.REJECT,
+                                 res.json.get("result").get("authentication"), res.json)
+                self.assertEqual("wrong otp value", detail.get("message"), detail)
+                lc.check_present(("privacyidea.lib.tokens.pushtoken", "WARNING",
+                                  "Unable to use 'require_presence' policy with 'push_wait'. "
+                                  "Disabling 'require_presence' policy!"))
+
+        remove_token(self.serial_push)
+        delete_policy("pol_push_config")
+        delete_policy("pol_push_require_presence")
+        delete_policy("pol_push_wait")
