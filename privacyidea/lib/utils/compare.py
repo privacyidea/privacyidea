b0725d2798d21c1532139528331ce60d7cf08b8c
Jona-Samuel Hoehmann <jona-samuel.hoehmann@netknights.it>
2026-04-30 10:00:14 +0200

    fix

--- privacyidea/lib/utils/compare.py
+++ privacyidea/lib/utils/compare.py
@@ -305,11 +305,13 @@ def negate(func: callable) -> callable:
     :param func: a comparison function taking the two values to compare as arguments
     :return: a comparison function taking the two values to compare as arguments
     """
+
     @wraps(func)
     def negated(left: any, right: any) -> bool:
         return not func(left, right)
 
     return negated
+
 
 # In order to add a comparator to this module, add a suitable member to PrimaryComparators and add an entry in the
 # COMPARATORS dictionary to map the name to the corresponding Comparator object containing the related function and
@@ -443,6 +445,7 @@ COMPARATORS = {PrimaryComparators.EQUALS: equals, "=": equals, "==": equals, "==
 COMPARATOR_DESCRIPTIONS = {comparator: COMPARATORS[comparator].description for comparator in
                            PrimaryComparators.get_all_comparators()}
 
+
 def get_all_type_restricted_comparators() -> list[str]:
     """
     Return a list of all type-restricted comparators.
@@ -567,7 +570,7 @@ def compare_ints(condition: str, value: Union[str, int]) -> bool:
     # Compare the values, but fail silent
     try:
         result = compare_values(value, condition.comparator, condition_value)
-    except CompareError as error: # pragma no cover
+    except CompareError as error:  # pragma no cover
         log.debug(f"Error during comparison: {error}")
         return False
     return result
@@ -623,8 +626,8 @@ def compare_generic(condition: str, key_method: callable, warning: str) -> bool:
         if not isinstance(left_value, int) and not isinstance(right_value, int):
             # try to convert both values to a timestamp
             try:
-                date1 = parse(left_value)
-                date2 = parse(right_value)
+                date1 = left_value if isinstance(left_value, datetime.datetime) else parse(left_value)
+                date2 = right_value if isinstance(right_value, datetime.datetime) else parse(right_value)
                 if date1 and date2:
                     # Only use dates, if both values can be converted to dates
                     left_value = date1
@@ -636,7 +639,7 @@ def compare_generic(condition: str, key_method: callable, warning: str) -> bool:
     try:
         # append "_any" to the comparator to indicate that we want to compare any type of value
         result = compare_values(left_value, comparator, right_value)
-    except CompareError as error:   # pragma no cover
+    except CompareError as error:  # pragma no cover
         log.debug(f"Error during comparison: {error}")
         return False
 
