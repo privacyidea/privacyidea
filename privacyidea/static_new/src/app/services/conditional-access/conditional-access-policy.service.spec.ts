07d7acfeeae0c31fcf929542a2b5583b3db69e21
Jelina Unger <jelina.unger@netknights.it>
2026-07-24 11:45:43 +0200

    fix test policy template

--- privacyidea/static_new/src/app/services/conditional-access/conditional-access-policy.service.spec.ts
+++ privacyidea/static_new/src/app/services/conditional-access/conditional-access-policy.service.spec.ts
@@ -44,6 +44,7 @@ describe("ConditionalAccessPolicyService", () => {
     dry_run: false,
     priority: 1,
     target: "user",
+    count_mode: "PER_REQUEST",
     counter_types_to_track: ["PIN_FAIL"],
     stages: [
       {
