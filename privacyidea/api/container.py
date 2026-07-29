442eea570d903999a47046e417d48ab032b4e868
Jelina Unger <jelina.unger@netknights.it>
2026-07-29 15:31:18 +0200

    write parameters to known audit column

--- privacyidea/api/container.py
+++ privacyidea/api/container.py
@@ -780,8 +780,7 @@ def set_container_info(container_serial, key):
 
     # Audit log
     g.audit_object.log({"container_serial": container_serial,
-                        "key": key,
-                        "value": value,
+                        "action_detail": f"key={key}, value={value}",
                         "success": res})
     return send_result(res)
 
